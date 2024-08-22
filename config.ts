2cd038e0fa873ef893bfcf32e9c7eb04b5110050
Leonardo Vieira <jonaledi@gmail.com>
2024-08-22 10:27:17 -0300

    refactor: requested changes in the pr

---
+++ config.ts
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+import dotenv from "dotenv";
+dotenv.config();
+export const config = {
+    production: {
+        networks: {
+            name: "mainnet",
+            chainId: 1,
+            rpcUrl1: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            rpcUrl2:
+                `https://rpc.ankr.com/eth/${process.env.ANKR_API_KEY}`,
+        },
+        contracts: {
+            ENSToken: {
+                address: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
+                startBlock: 13533418,
+            },
+            ENSGovernor: {
+                address: "0x323a76393544d5ecca80cd6ef2a560c6a395b7e3",
+                startBlock: 13533772,
+            },
+        },
+    },
+    staging: {
+        networks: {
+            name: "mainnet",
+            chainId: 1,
+            rpcUrl1: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            rpcUrl2:
+                `https://rpc.ankr.com/eth/${process.env.ANKR_API_KEY}`,
+        },
+        contracts: {
+            ENSToken: {
+                address: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
+                startBlock: 13533418,
+            },
+            ENSGovernor: {
+                address: "0x323a76393544d5ecca80cd6ef2a560c6a395b7e3",
+                startBlock: 13533772,
+            },
+        },
+    },
+};
--- ponder.config.ts
+++ ponder.config.ts
@@ -2,42 +2,33 @@ import { createConfig, loadBalance } from "@ponder/core";
 import { http } from "viem";
 import { ENSTokenAbi } from "./abis/ENSTokenAbi";
 import { ENSGovernorAbi } from "./abis/ENSGovernorAbi";
-import configData from "./config.json";
 import dotenv from "dotenv";
-
+import { config } from "./config";
 dotenv.config();
 
 let networks, contracts;
 if (!process.env.STATUS) {
   throw new Error("Env variable STATUS is not defined");
 } else if (process.env.STATUS === "production") {
-  ({ networks, contracts } = configData["production"]);
+  ({ networks, contracts } = config["production"]);
 } else {
-  ({ networks, contracts } = configData["staging"]);
-}
-
-let startBlock = 0;
-if (process.env.START_BLOCK) {
-  startBlock = parseInt(process.env.START_BLOCK);
+  ({ networks, contracts } = config["staging"]);
 }
 
 export default createConfig({
   networks: {
     mainnet: {
       chainId: networks.chainId,
-      transport: loadBalance([
-        http(process.env.PONDER_RPC_URL_1),
-        http(process.env.PONDER_RPC_URL_1_2),
-      ]),
+      transport: loadBalance([http(networks.rpcUrl1), http(networks.rpcUrl2)]),
     },
   },
+
   contracts: {
     ENSToken: {
       abi: ENSTokenAbi,
       address: contracts.ENSToken.address as `0x${string}`,
       network: networks.name as any,
       startBlock: contracts.ENSToken.startBlock,
-
     },
     ENSGovernor: {
       abi: ENSGovernorAbi,
