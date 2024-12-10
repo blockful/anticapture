963d0140f56d72aeb7243b9e497bdb55edbd5b03
heronlancellot <heronlancellot@id.uff.br>
2024-12-10 14:54:42 -0300

    feat: add quorum-card

---
+++ apps/dashboard/components/01-atoms/QuorumCard.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { UsersIcon, InfoIcon } from "@/components/01-atoms";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@radix-ui/react-tooltip";
+
+export const QuorumCard = () => {
+  return (
+    <div className="card-container-about">
+      <div className="card-header-about">
+        <UsersIcon />
+        <h1 className="card-header-about-text">Quorum</h1>
+      </div>
+      <div className="card-body-about">
+        <div className="card-description-about">
+          <div className="card-description-title">
+            <h1 className="text-foreground">Logic</h1>
+            <Tooltip>
+              <TooltipTrigger>
+                <InfoIcon />
+              </TooltipTrigger>
+              <TooltipContent className="m-1 border-foreground bg-dark">
+                <p className="text-white">
+                  Direct liquid profit: Cost of direct capture
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+          <div className="flex h-full w-full items-center justify-start gap-1.5">
+            <div className="flex w-1/2">
+              <p className="flex text-sm font-medium leading-tight">
+                For + Abstain
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card-description-about">
+          <div className="card-description-title">
+            <h1 className="text-foreground">Proposal threshold</h1>
+            <Tooltip>
+              <TooltipTrigger>
+                <InfoIcon />
+              </TooltipTrigger>
+              <TooltipContent className="m-1 border-foreground bg-dark">
+                <p className="text-white">
+                  Direct liquid profit: Cost of direct capture
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+          <div className="flex h-full w-full items-center justify-start gap-1.5">
+            <div className="flex w-1/2">
+              <p className="flex text-sm font-medium leading-tight">5%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
