ab72367171492a9d0a344e08e7c2336ff611823c
heronlancellot <heronlancellot@id.uff.br>
2024-12-11 12:27:34 -0300

    feat: add the-section in home-template

--- apps/dashboard/components/04-templates/HomeTemplate.tsx
+++ apps/dashboard/components/04-templates/HomeTemplate.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import { AboutUniswapSection } from "@/components/03-organisms";
 import { AttacksSection } from "../ui/attacks-section";
 import { DelegatesTable } from "../ui/delegates-table";
 import { HoldersTable } from "../ui/holders-table";
 import { ProfitabilitySection } from "../ui/profitability-section";
 import { SupplySection } from "../ui/supply-section";
 import { UncertaintySection } from "../ui/uncertainty-section";
+import { UniswapIcon } from "@/components/01-atoms";
+import { UniswapDaoInfo } from "@/components/02-molecules";
+import { TheSection } from "@/components/03-organisms";
 
 export const HomeTemplate = () => {
   return (
     <main className="mx-auto flex flex-col items-center gap-8 px-8 py-6 lg:gap-16 xl:overflow-auto">
-      <AboutUniswapSection />
+      <TheSection
+        title="Uniswap DAO Info"
+        icon={<UniswapIcon className="text-foreground" />}
+      >
+        <UniswapDaoInfo />
+      </TheSection>
       <div className="grid w-full grid-cols-1 grid-rows-[183px_auto] gap-4 md:grid-cols-2">
         <ProfitabilitySection />
         <UncertaintySection />
