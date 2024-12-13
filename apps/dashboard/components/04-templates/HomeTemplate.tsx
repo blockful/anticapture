b9cb37a031511c87f09a41b57f71bd35e6ef9897
heronlancellot <heronlancellot@id.uff.br>
2024-12-13 15:34:25 -0300

    fix: adjust home-template

--- apps/dashboard/components/04-templates/HomeTemplate.tsx
+++ apps/dashboard/components/04-templates/HomeTemplate.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { AttacksSection } from "../ui/attacks-section";
 import { DelegatesTable } from "../ui/delegates-table";
 import { HoldersTable } from "../ui/holders-table";
-import { ProfitabilitySection } from "../ui/profitability-section";
-import { SupplySection } from "../ui/supply-section";
-import { UncertaintySection } from "../ui/uncertainty-section";
 import { UniswapIcon } from "@/components/01-atoms";
 import { UniswapDaoInfo } from "@/components/02-molecules";
 import { TheSection } from "@/components/03-organisms";
@@ -19,12 +15,7 @@ export const HomeTemplate = () => {
       >
         <UniswapDaoInfo />
       </TheSection>
-      <div className="grid w-full grid-cols-1 grid-rows-[183px_auto] gap-4 md:grid-cols-2">
-        <ProfitabilitySection />
-        <UncertaintySection />
-        <AttacksSection />
-        <SupplySection />
-      </div>
+
       <div className="mt-4 grid w-full grid-cols-1 grid-rows-[auto] gap-4 xl:grid-cols-2">
         <HoldersTable />
         <DelegatesTable />
