9ba53abd70e3e7ca8492c2f3b42f38f5ff534b24
heronlancellot <heronlancellot@id.uff.br>
2024-12-13 16:40:28 -0300

    feat: add arrow-left-right

--- apps/dashboard/components/01-atoms/index.ts
+++ apps/dashboard/components/01-atoms/index.ts
@@ -5,6 +5,7 @@ export * from "./ContractsCard";
 export * from "./QuorumCard";
 export * from "./Skeleton";
 export * from "./Switcher";
+export * from "./SwitcherDate";
 export * from "./TimelockCard";
 export * from "./TooltipInfo";
 export * from "./VoteCard";
