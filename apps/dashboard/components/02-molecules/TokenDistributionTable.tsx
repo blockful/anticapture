cb02db11a952d4f7da257acd8d3375666d9f45e7
heronlancellot <heronlancellot@id.uff.br>
2024-12-18 09:55:08 -0300

    feat: add variation total-supply %

--- apps/dashboard/components/02-molecules/TokenDistributionTable.tsx
+++ apps/dashboard/components/02-molecules/TokenDistributionTable.tsx
@@ -197,7 +197,7 @@ export const TokenDistributionTable = ({
             amount: String(
               BigInt(result.currentTotalSupply) / BigInt(10 ** 18),
             ),
-            variation: result.changeRate,
+            variation: `${Number(Number(result.changeRate) * 100).toFixed(2)}%`,
           },
         });
       })
