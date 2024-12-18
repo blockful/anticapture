a94f88dd618841fe8b9a6473c85061b6155e5a85
heronlancellot <heronlancellot@id.uff.br>
2024-12-18 03:18:19 -0300

    feat: add apple-icon

---
+++ apps/dashboard/components/01-atoms/icons/AppleIcon.tsx
@@ -0,0 +1,38 @@
+import { SVGProps } from "react";
+
+export const AppleIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      {...props}
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="apple" clip-path="url(#clip0_5322_2420)">
+        <path
+          id="Vector"
+          d="M7.99998 13.9599C8.99998 13.9599 9.83331 14.6666 10.6666 14.6666C12.6666 14.6666 14.6666 9.33322 14.6666 6.51989C14.6439 5.65637 14.2808 4.83684 13.6564 4.23992C13.032 3.643 12.197 3.31709 11.3333 3.33322C9.85331 3.33322 8.66665 4.29322 7.99998 4.66655C7.33331 4.29322 6.14665 3.33322 4.66665 3.33322C3.80248 3.31533 2.96645 3.64065 2.34167 4.23794C1.71689 4.83523 1.3543 5.65579 1.33331 6.51989C1.33331 9.33322 3.33331 14.6666 5.33331 14.6666C6.16665 14.6666 6.99998 13.9599 7.99998 13.9599Z"
+          stroke="#FAFAFA"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M6.66669 1.3335C7.33335 1.66683 8.00002 2.66683 8.00002 4.66683"
+          stroke="#FAFAFA"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5322_2420">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
