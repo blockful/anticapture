c79e3406b85040987ce0f15527385597d10aeb0f
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-01-10 12:12:47 +0000

    feat: add bigint operations in utils

--- apps/indexer/src/lib/utils.ts
+++ apps/indexer/src/lib/utils.ts
@@ -25,3 +25,30 @@ export const getValueFromEventArgs = <T, K extends { [k: string]: any }>(
 export const convertSecondsTimestampToDate = (timestamp: number) => {
   return new Date(parseInt(String(timestamp) + "000"));
 };
+
+/**
+ * Calculates the absolute difference between two numbers
+ */
+export function delta(a: bigint, b: bigint): bigint {
+  return a > b ? a - b : b - a;
+}
+
+/**
+ * Returns the minimum of two or more numbers
+ */
+export function min(...values: bigint[]): bigint {
+  if (values.length === 0) {
+    throw new Error('At least one value must be provided');
+  }
+  return values.reduce((min, value) => value < min ? value : min);
+}
+
+/**
+ * Returns the maximum of two or more numbers
+ */
+export function max(...values: bigint[]): bigint {
+  if (values.length === 0) {
+    throw new Error('At least one value must be provided');
+  }
+  return values.reduce((max, value) => value > max ? value : max);
+}
