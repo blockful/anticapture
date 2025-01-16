a1ddfa1f635ac4ed5cd6d12d4d17f87a01d55b57
heronlancellot <heronlancellot@id.uff.br>
2025-01-16 19:18:45 -0300

    update: the-chart

--- apps/dashboard/components/01-atoms/TheChart.tsx
+++ apps/dashboard/components/01-atoms/TheChart.tsx
@@ -1,56 +1,42 @@
 "use client";
 
-import { Area, AreaChart, ResponsiveContainer, XAxis } from "recharts";
 import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { ChartMetrics, chartMetrics } from "@/lib/mocked-data";
 
 const transformChartMetrics = (data: ChartMetrics[]) => {
   return data.map((item: ChartMetrics) => ({
-    date: new Date(Number(item.dayTimestamp)).toISOString().split("T")[0],
+    date: new Date(Number(item.date)).toISOString().split("T")[0],
     high: Number(item.high),
   }));
 };
 
 const formattedChartMetrics = transformChartMetrics(chartMetrics);
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
+console.log("formattedChartMetrics", formattedChartMetrics);
 
 export const TheChart = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <ChartContainer className="h-full w-full" config={chartConfig}>
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart data={formattedChartMetrics}>
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tick={false}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="dot" hideLabel />}
-            />
-            <Area
-              dataKey="high"
-              type="linear"
-              fill="var(--color-desktop)"
-              fillOpacity={0.4}
-              stroke="var(--color-desktop)"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </ChartContainer>
+    <div className="flex h-14 w-full items-center justify-center">
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart data={formattedChartMetrics}>
+          <XAxis tickLine={false} axisLine={false} tick={false} />
+          <YAxis tickLine={false} axisLine={false} tick={false} />
+          <Tooltip />
+          <Area
+            dataKey="high"
+            type="linear"
+            fill="#fff"
+            fillOpacity={0.4}
+            stroke="#fff"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 };
