e17ad72db2d3ed4df1b2899cc3137acdd368e63b
heronlancellot <heronlancellot@id.uff.br>
2025-02-26 16:20:54 -0300

    feat: add switcher-chart

---
+++ apps/dashboard/components/01-atoms/SwitcherChart.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+interface SwitcherChartProps {
+  setMetric: (metric: string) => void;
+  defaultValue: string;
+  options: string[];
+}
+
+export const SwitcherChart = ({
+  setMetric,
+  defaultValue,
+  options,
+}: SwitcherChartProps) => {
+  return (
+    <Tabs defaultValue={defaultValue} className="gap-1 rounded-md">
+      <TabsList>
+        {options.map((option) => (
+          <TabsTrigger
+            key={option}
+            value={option}
+            onClick={() => setMetric(option)}
+          >
+            {option}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+    </Tabs>
+  );
+};
