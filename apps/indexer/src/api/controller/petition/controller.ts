705e93ef471971d8c381aad3ddd505ab3fbd7a97
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2025-03-10 18:07:40 -0300

    feat: Add total supporters, supporters voting power (sum) and latest 10 supporters in the petition endpoint return obj; fix: Fixing response type;

--- apps/indexer/src/api/controller/petition/controller.ts
+++ apps/indexer/src/api/controller/petition/controller.ts
@@ -2,28 +2,53 @@ import { db, schema } from "@/offchain/db.schema";
 import { isPetitionSignature, PetitionSignature } from "./types";
 import { Hono } from "hono";
 import { Hex, verifyMessage } from "viem";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
 const app = new Hono();
 
 app.get("/petition/:daoId", async (context) => {
   // Getting the daoId from the url
   const daoId = context.req.param("daoId");
+  const userAddress = context.req.query("userAddress");
   // Getting the petition signatures from the db
   const petitionSignatures = await db
-    .select()
+    .select({
+      accountId: schema.petitionSignatures.accountId,
+      daoId: schema.petitionSignatures.daoId,
+      timestamp: schema.petitionSignatures.timestamp,
+      message: schema.petitionSignatures.message,
+      signature: schema.petitionSignatures.signature,
+      votingPower: schema.accountPower.votingPower,
+    })
     .from(schema.petitionSignatures)
     .leftJoin(
       schema.account,
-      eq(schema.petitionSignatures.accountId, schema.account.id),
+      eq(schema.petitionSignatures.accountId, schema.account.id)
     )
     .leftJoin(
       schema.accountPower,
-      eq(schema.account.id, schema.accountPower.accountId),
+      eq(schema.account.id, schema.accountPower.accountId)
     )
-    .where(eq(schema.petitionSignatures.daoId, daoId));
+    .where(eq(schema.petitionSignatures.daoId, daoId))
+    .orderBy(desc(schema.petitionSignatures.timestamp));
+  const petetionSignaturesResponse = {
+    petitionSignatures,
+    totalSignatures: petitionSignatures.length,
+    totalSignaturesPower: petitionSignatures.reduce(
+      (acc, curr) => acc + (curr.votingPower ?? 0n),
+      0n,
+    ),
+    latestVoters: petitionSignatures
+      .map(({ accountId }) => accountId)
+      .slice(0, 10),
+    userSigned: !!userAddress
+      ? petitionSignatures.find(
+          (signature) => signature.accountId === userAddress
+        ) !== undefined
+      : undefined,
+  };
   // Returning the petition signatures
-  return context.json(petitionSignatures);
+  return context.json(petetionSignaturesResponse);
 });
 
 app.post("/petition", async (context) => {
