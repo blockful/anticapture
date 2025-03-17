18393aa774963b48924771cc38a4eed53c7816de
heronlancellot <heronlancellot@id.uff.br>
2025-03-17 11:29:56 -0300

    update: cad to be opened h-full

--- apps/dashboard/components/atoms/ExtractableValueAccordion.tsx
+++ apps/dashboard/components/atoms/ExtractableValueAccordion.tsx
@@ -91,13 +91,13 @@ export const ExtractableValueAccordion = () => {
     <Accordion
       type="single"
       collapsible
-      className="flex w-full flex-col gap-3 text-white"
+      className="flex h-full w-full flex-col gap-3 text-white"
     >
       {AccordionData.map((item, index) => (
         <AccordionItem
           key={index}
           value={`item-${index}`}
-          className="group flex w-full flex-col rounded-lg border border-white/10 bg-dark p-3 hover:bg-[#26262A] data-[state=open]/trigger:gap-4"
+          className="group flex w-full flex-col rounded-lg border border-white/10 bg-dark p-3 hover:bg-[#26262A] data-[state=open]:flex-1 data-[state=open]:gap-4"
         >
           <AccordionTrigger className="group/trigger flex w-full items-center justify-between">
             <div className="flex items-center gap-2 text-sm font-medium text-foreground group-hover:text-white group-data-[state=open]/trigger:text-white">
@@ -106,7 +106,7 @@ export const ExtractableValueAccordion = () => {
             </div>
             <PlusIcon className="h-4 w-4 text-foreground transition-transform duration-200 group-data-[state=open]/trigger:rotate-45" />
           </AccordionTrigger>
-          <AccordionContent className="flex flex-col gap-2 text-sm font-normal text-foreground">
+          <AccordionContent className="flex flex-1 flex-col gap-2 text-sm font-normal text-foreground">
             {item.content}
           </AccordionContent>
         </AccordionItem>
