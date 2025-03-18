4b7e7703bf234a0b483cf46589bc2ae568033f82
heronlancellot <heronlancellot@id.uff.br>
2025-03-18 17:39:22 -0300

    feat: add new style to switcher-date

--- apps/dashboard/components/atoms/SwitcherDate.tsx
+++ apps/dashboard/components/atoms/SwitcherDate.tsx
@@ -5,10 +5,10 @@ import { TimeInterval } from "@/lib/enums/TimeInterval";
 
 export const SwitcherDate = ({
   setTimeInterval,
-  defaultValue
+  defaultValue,
 }: {
   setTimeInterval: (timeInterval: TimeInterval) => void;
-  defaultValue: TimeInterval
+  defaultValue: TimeInterval;
 }) => {
   /**
    * This function is called when a new date is selected.
@@ -19,24 +19,28 @@ export const SwitcherDate = ({
     <Tabs defaultValue={defaultValue}>
       <TabsList>
         <TabsTrigger
+          className="min-w-[72px] px-1.5 py-0.5 text-sm font-medium"
           value={TimeInterval.SEVEN_DAYS}
           onClick={() => setTimeInterval(TimeInterval.SEVEN_DAYS)}
         >
           {TimeInterval.SEVEN_DAYS}
         </TabsTrigger>
         <TabsTrigger
+          className="min-w-[72px] px-1.5 py-0.5 text-sm font-medium"
           value={TimeInterval.THIRTY_DAYS}
           onClick={() => setTimeInterval(TimeInterval.THIRTY_DAYS)}
         >
           {TimeInterval.THIRTY_DAYS}
         </TabsTrigger>
         <TabsTrigger
+          className="min-w-[72px] px-1.5 py-0.5 text-sm font-medium"
           value={TimeInterval.NINETY_DAYS}
           onClick={() => setTimeInterval(TimeInterval.NINETY_DAYS)}
         >
           {TimeInterval.NINETY_DAYS}
         </TabsTrigger>
         <TabsTrigger
+          className="min-w-[72px] px-1.5 py-0.5 text-sm font-medium"
           value={TimeInterval.ONE_YEAR}
           onClick={() => setTimeInterval(TimeInterval.ONE_YEAR)}
         >
