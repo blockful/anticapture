f972e7c24ec3c5100f76b88b1683ae628b76aaff
heronlancellot <heronlancellot@id.uff.br>
2025-03-21 09:12:38 -0300

    update: base-header-layout-sidebar to don't appear when is mobile

--- apps/dashboard/components/atoms/BaseHeaderLayoutSidebar.tsx
+++ apps/dashboard/components/atoms/BaseHeaderLayoutSidebar.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { toggleScreenScroll } from "@/lib/client/utils";
 import { ConnectWallet } from "@/components/atoms";
+import { useScreenSize } from "@/lib/hooks/useScreenSize";
+import { HeaderMobile } from "./HeaderMobile";
 
 export const BaseHeaderLayoutSidebar = ({
   children,
@@ -14,19 +16,19 @@ export const BaseHeaderLayoutSidebar = ({
    * Below logic is only used for screens that are smaller than 1200px wide
    */
   const [displaySidebar, setDisplaySidebar] = useState<boolean>(false);
-
+  const { isMobile, isTablet } = useScreenSize();
   const toggleSidebar = () => {
     toggleScreenScroll();
     setDisplaySidebar(!displaySidebar);
   };
 
-  return (
+  return isMobile && !isTablet ? (
+    <HeaderMobile />
+  ) : (
     <>
       <button
         onClick={toggleSidebar}
-        className={`group fixed left-6 top-6 z-50 rounded-full border border-middleDark bg-darkest p-2 text-xs transition hover:bg-dark xl:hidden ${
-          displaySidebar && "translate-x-[284px]"
-        }`}
+        className={`group fixed left-6 top-6 z-50 rounded-full border border-middleDark bg-darkest p-2 text-xs transition hover:bg-dark xl:hidden ${displaySidebar && "translate-x-[284px]"}`}
       >
         {displaySidebar ? (
           <ChevronLeft className="h-4 w-4 text-middleDark group-hover:text-foreground" />
@@ -34,11 +36,8 @@ export const BaseHeaderLayoutSidebar = ({
           <ChevronRight className="h-4 w-4 text-middleDark group-hover:text-foreground" />
         )}
       </button>
-
       <header
-        className={`fixed left-0 top-0 z-40 flex h-screen w-[330px] flex-col items-start justify-start border border-middleDark bg-dark shadow-lg transition-transform xl:absolute xl:translate-x-0 ${
-          displaySidebar ? "translate-x-0" : "-translate-x-[354px]"
-        }`}
+        className={`fixed left-0 top-0 z-40 flex h-screen w-[330px] flex-col items-start justify-start border border-middleDark bg-dark shadow-lg transition-transform xl:absolute xl:translate-x-0 ${displaySidebar ? "translate-x-0" : "-translate-x-[354px]"}`}
       >
         <div className="flex h-full w-full flex-col justify-between">
           <div>{children}</div>
