742db43e895c62d877f9bc4ca457af9431a697d5
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2025-03-24 17:40:04 -0300

    feat: finishing ReachOutToUsCard responsivity fixes;

--- apps/dashboard/components/molecules/ReachOutToUsCard.tsx
+++ apps/dashboard/components/molecules/ReachOutToUsCard.tsx
@@ -7,25 +7,27 @@ import { Card } from "@/components/ui/card";
 export const ReachOutToUsCard = () => {
   return (
     <Card
-      className="flex w-full flex-row rounded-lg border border-lightDark bg-dark px-4 py-5 shadow hover:cursor-pointer hover:bg-brandOrange/15 xl4k:max-w-full"
+      className="flex w-full rounded-lg border border-lightDark bg-dark px-4 md:py-5 py-3 shadow hover:cursor-pointer hover:bg-tangerine/15 xl4k:max-w-full"
       onClick={() => {
         window.open("https://tally.so/r/nrvGbv", "_blank");
       }}
     >
-      <div className="flex w-full flex-row justify-between">
+      <div className="flex w-full justify-between">
         <div className="flex flex-row items-center">
           <div className="flex items-center gap-2">
             <div className="flex h-6 w-6 items-center justify-center rounded-full bg-lightDark">
               <SimpleGlobeIcon className="h-4 w-4 text-foreground" />
             </div>
-            <h3 className="text-md font-small pl-1 text-white">
-              Don&apos;t see your DAO here?
-            </h3>
-            <div className="flex items-center gap-1">
-              <h3 className="text-md font-small text-brandOrange">
-                Reach out to us
+            <div className="flex flex-col md:flex-row items-start justify-start gap-0 md:gap-2">
+              <h3 className="md:text-md text-sm font-small md:pl-1 pl-0 text-white ">
+                Don&apos;t see your DAO here?
               </h3>
-              <ArrowRightIcon className="h-4 w-4 pl-0 text-brandOrange" />
+              <div className="flex items-center gap-1">
+                <h3 className="md:text-md text-sm font-small text-tangerine">
+                  Reach out to us
+                </h3>
+                <ArrowRightIcon className="h-4 w-4 pl-0 text-tangerine" />
+              </div>
             </div>
           </div>
         </div>
