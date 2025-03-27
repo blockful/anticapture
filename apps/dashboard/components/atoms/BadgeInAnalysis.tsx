f297c1db895832ff0d3c826b1985fd0733449d7e
Luiz Gustavo Abou Hatem de Liz <50002794+lgahdl@users.noreply.github.com>
2025-03-27 09:17:01 -0300

    Update apps/dashboard/components/atoms/BadgeInAnalysis.tsx
    Co-authored-by: heronlancellot.eth <heronlancellot@gmail.com>

--- apps/dashboard/components/atoms/BadgeInAnalysis.tsx
+++ apps/dashboard/components/atoms/BadgeInAnalysis.tsx
@@ -3,15 +3,10 @@ import { Clock2 } from "lucide-react";
 
 export const BadgeInAnalysis = () => {
   const { isMobile } = useScreenSize();
-  return isMobile ? (
+  return (
     <div className="flex w-fit items-center gap-2 rounded-full bg-white/10 px-1.5 py-0.5 text-foreground">
       <Clock2 className="h-4 w-4" />
-      In analysis
+     {isMobile ? 'In analysis' : 'Analysis in progress'}
     </div>
-  ) : (
-    <div className="flex w-fit items-center gap-2 rounded-full bg-white/10 px-1.5 py-0.5 text-foreground">
-      <Clock2 className="h-4 w-4" />
-      Analysis in progress
-    </div>
-  );
-};
+  ) 
+
