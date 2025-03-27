73f06b01172564d5cfdd67dd36519fd33e27f065
heronlancellot <heronlancellot@id.uff.br>
2025-03-27 12:34:09 -0300

    fix: header-nav-mobile

--- apps/dashboard/components/molecules/HeaderNavMobile.tsx
+++ apps/dashboard/components/molecules/HeaderNavMobile.tsx
@@ -2,9 +2,28 @@
 
 import { SECTIONS_CONSTANTS } from "@/lib/constants";
 import { ButtonHeaderDAOSidebarMobile } from "@/components/atoms";
+import daoConstantsByDaoId from "@/lib/dao-constants";
+import { DaoConstantsFullySupported } from "@/lib/dao-constants/types";
+import { DaoIdEnum, SUPPORTED_DAO_NAMES } from "@/lib/types/daos";
+import { usePathname } from "next/navigation";
+
+interface NavOption {
+  anchorId: string;
+  title: string;
+}
 
 export const HeaderNavMobile = () => {
-  const options = [
+  const pathname = usePathname();
+  const isDefault = pathname === "/";
+  const daoId = isDefault ? null : pathname.split("/")[1]?.toUpperCase();
+  const isValidDao = daoId && SUPPORTED_DAO_NAMES.includes(daoId as DaoIdEnum);
+
+  const hasGovernanceImplementation =
+    isValidDao &&
+    !!(daoConstantsByDaoId[daoId as DaoIdEnum] as DaoConstantsFullySupported)
+      .governanceImplementation;
+
+  const options: NavOption[] = [
     {
       anchorId: SECTIONS_CONSTANTS.daoInfo.anchorId,
       title: SECTIONS_CONSTANTS.daoInfo.title,
@@ -13,10 +32,14 @@ export const HeaderNavMobile = () => {
       anchorId: SECTIONS_CONSTANTS.attackProfitability.anchorId,
       title: SECTIONS_CONSTANTS.attackProfitability.title,
     },
-    {
-      anchorId: SECTIONS_CONSTANTS.governanceImplementation.anchorId,
-      title: SECTIONS_CONSTANTS.governanceImplementation.title,
-    },
+    ...(hasGovernanceImplementation
+      ? [
+          {
+            anchorId: SECTIONS_CONSTANTS.governanceImplementation.anchorId,
+            title: SECTIONS_CONSTANTS.governanceImplementation.title,
+          },
+        ]
+      : []),
     {
       anchorId: SECTIONS_CONSTANTS.tokenDistribution.anchorId,
       title: SECTIONS_CONSTANTS.tokenDistribution.title,
