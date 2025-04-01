af0c65f55233e4caeddc8b452e5a6b8182cec593
heronlancellot <heronlancellot@id.uff.br>
2025-04-01 16:31:56 -0300

    update: dao-info-section

--- apps/dashboard/components/organisms/DaoInfoSection.tsx
+++ apps/dashboard/components/organisms/DaoInfoSection.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { GlobeIcon, TheSectionLayout } from "@/components/atoms";
+import Image from "next/image";
+import {
+  CrownIcon,
+  TokensIcon,
+  DaoInfoDropdown,
+  FocusIcon,
+} from "@/components/atoms";
 import {
   ContractsCard,
   QuorumCard,
@@ -8,15 +14,45 @@ import {
   TimelockCard,
   VoteCard,
 } from "@/components/molecules";
-import { SECTIONS_CONSTANTS } from "@/lib/constants";
-import daoConstantsByDaoId from "@/lib/dao-constants";
+import { FilePenLine, LinkIcon } from "lucide-react";
 import { DaoIdEnum } from "@/lib/types/daos";
+import daoConstantsByDaoId from "@/lib/dao-constants";
+import { openEtherscanAddress } from "@/lib/utils/openEtherscanAddress";
 
 export const DaoInfoSection = ({ daoId }: { daoId: DaoIdEnum }) => {
   const daoConstants = daoConstantsByDaoId[daoId];
+
   if (daoConstants.inAnalysis) {
     return null;
   }
+
+  const onChainOptions = [
+    {
+      value: "Governor",
+      icon: <CrownIcon className="text-tangerine" />,
+      onClick: () => openEtherscanAddress(daoConstants.contracts.governor),
+    },
+    {
+      value: "Token",
+      icon: <TokensIcon className="text-tangerine" />,
+      onClick: () => openEtherscanAddress(daoConstants.contracts.token),
+    },
+  ];
+
+  const offChainOptions = [
+    {
+      value: "Snapshot",
+      icon: <FocusIcon className="text-tangerine" />,
+      onClick: () =>
+        window.open(daoConstants.snapshot, "_blank", "noopener,noreferrer"),
+    },
+    {
+      value: "Token",
+      icon: <TokensIcon className="text-tangerine" />,
+      onClick: () => openEtherscanAddress(daoConstants.contracts.token),
+    },
+  ];
+
   const DaoInfo = () => {
     return (
       <div className="grid w-full gap-2 text-white md:grid-cols-2 xl:gap-4">
@@ -30,13 +66,43 @@ export const DaoInfoSection = ({ daoId }: { daoId: DaoIdEnum }) => {
   };
 
   return (
-    <TheSectionLayout
-      title={SECTIONS_CONSTANTS.daoInfo.title}
-      icon={<GlobeIcon />}
-      anchorId={SECTIONS_CONSTANTS.daoInfo.anchorId}
-      className="gap-5 border-b-2 border-b-white/10 px-4 pb-8 pt-10 sm:gap-6 sm:px-0 sm:pb-0 sm:pt-0"
-    >
-      <DaoInfo />
-    </TheSectionLayout>
+    <section className="flex h-full w-full flex-col rounded-md border-lightDark bg-dark px-4 pb-8 pt-10 sm:px-0 sm:pb-0 sm:pt-0">
+      <div className="flex gap-3.5 p-4 sm:gap-5">
+        <div className="flex">
+          <Image
+            className="overflow-hidden rounded-full"
+            src={daoConstants.icon}
+            alt={"OK"}
+            width={72}
+            height={72}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <h2 className="text-[24px] font-semibold leading-8 text-[#FAFAFA]">
+              {daoConstants.name}
+            </h2>
+          </div>
+          <div className="flex gap-2">
+            <DaoInfoDropdown
+              defaultValue={{
+                value: "OnChain Gov",
+                icon: <LinkIcon className="size-3.5 text-[#FAFAFA]" />,
+                onClick: () => {},
+              }}
+              options={onChainOptions}
+            />
+            <DaoInfoDropdown
+              defaultValue={{
+                value: "OffChain Gov",
+                icon: <FilePenLine className="size-3.5 text-[#FAFAFA]" />,
+                onClick: () => {},
+              }}
+              options={offChainOptions}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
