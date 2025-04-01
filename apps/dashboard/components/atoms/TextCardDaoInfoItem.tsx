923dc1afd83ebb6fc6802ab351dbae77e3541756
heronlancellot <heronlancellot@id.uff.br>
2025-04-01 18:24:45 -0300

    update: text-card-dao-info with bg and customClassName

--- apps/dashboard/components/atoms/TextCardDaoInfoItem.tsx
+++ apps/dashboard/components/atoms/TextCardDaoInfoItem.tsx
@@ -5,9 +5,17 @@ interface TextItemProps {
   value?: string | number;
 }
 
-export const TextCardDaoInfoItem = (item: TextItemProps) => {
+export const TextCardDaoInfoItem = ({
+  item,
+  className,
+}: {
+  item: TextItemProps;
+  className?: string;
+}) => {
   return (
-    <p className="flex h-full w-full text-sm font-medium leading-tight">
+    <p
+      className={`flex h-full w-full rounded-lg bg-lightDark px-2 py-1 text-sm font-medium leading-tight text-[#FAFAFA] ${className}`}
+    >
       {item.label} {item.value}
     </p>
   );
