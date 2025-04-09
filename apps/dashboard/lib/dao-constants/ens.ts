8f877a930d14a57599f15191a02cf2bc30a19352
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2025-04-09 09:43:45 -0300

    fix: Fixing the cancel function redirect on ENS.

--- apps/dashboard/lib/dao-constants/ens.ts
+++ apps/dashboard/lib/dao-constants/ens.ts
@@ -12,13 +12,13 @@ export const ENS: DaoConstants = {
     token: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
     timelock: "0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7",
   },
-  cancelFunction: undefined,
+  cancelFunction: "https://etherscan.io/address/0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7#writeContract#F1",
   snapshot: "https://snapshot.box/#/s:ens.eth",
   rules: {
     delay: true,
     changeVote: false,
     timelock: true,
-    cancelFunction: false,
+    cancelFunction: true,
   },
   supportsLiquidTreasuryCall: true,
   securityCouncil: {
