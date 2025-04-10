d05ecc0aaa7fa23a7c5d25dc6a4300d99034bdda
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2025-04-10 15:39:47 -0300

    fix: fixing type errors;

--- apps/dashboard/lib/dao-config/uni.ts
+++ apps/dashboard/lib/dao-config/uni.ts
@@ -9,7 +9,6 @@ export const UNI: DaoConfiguration = {
   icon: UNILogo,
   supportStage: SupportStageEnum.FULL,
   daoInfo: {
-    
     contracts: {
       governor: "0x408ED6354d4973f66138C91495F2f2FCbd8724C3",
       token: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
@@ -26,18 +25,9 @@ export const UNI: DaoConfiguration = {
     },
   },
   attackProfitability: {
-    
     riskLevel: RiskLevel.LOW,
     supportsLiquidTreasuryCall: false,
   },
-  governanceImplementation: false,
-  tokenDistribution: {
-    
-  },
-  governanceActivity: {
-    
-  },
-  showSupport: {
-    
-  },
+  tokenDistribution: true,
+  governanceActivity: true,
 };
