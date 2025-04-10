eb2abbd84b41b1186c7816fd50ebf423e0b94ec4
lucas picollo <lucaspicolloo@gmail.com>
2025-04-10 15:57:55 -0300

    feat: create config module with zod

---
+++ apps/indexer/confg.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  STATUS: z.enum(["production", "staging", "test"]),
+  DATABASE_URL: z.string(),
+  PONDER_RPC_URL: z.string(),
+  POOLING_INTERVAL: z.number().default(1000),
+  MAX_REQUESTS_PER_SECOND: z.number().default(50),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
