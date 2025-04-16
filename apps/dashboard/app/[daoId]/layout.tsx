e732359712f0ec9c7438b9c08568c67bd9122a52
heronlancellot <heronlancellot@id.uff.br>
2025-04-16 09:50:31 -0300

    fix: remove unused commented

--- apps/dashboard/app/[daoId]/layout.tsx
+++ apps/dashboard/app/[daoId]/layout.tsx
@@ -1,14 +1,13 @@
+import { ReactNode } from "react";
 import { ALL_DAOS, DaoIdEnum } from "@/lib/types/daos";
-import { SupportStageEnum } from "@/lib/enums/SupportStageEnum";
 import { DaoDataProvider } from "@/contexts/DaoDataContext";
 import NotFound from "@/app/[daoId]/not-found";
-import { HeaderDAOSidebar } from "@/components/molecules";
 import { TokenDistributionProvider } from "@/contexts/TokenDistributionContext";
 import { GovernanceActivityProvider } from "@/contexts/GovernanceActivityContext";
 import daoConfigByDaoId from "@/lib/dao-config";
 
 interface DaoLayoutProps {
-  children: React.ReactNode;
+  children: ReactNode;
   params: { daoId: string };
 }
 
@@ -31,7 +30,6 @@ export default function DaoLayout({ children, params }: DaoLayoutProps) {
     <DaoDataProvider daoId={daoId}>
       <TokenDistributionProvider daoId={daoId}>
         <GovernanceActivityProvider daoId={daoId}>
-          {/* <HeaderDAOSidebar /> */}
           {children}
         </GovernanceActivityProvider>
       </TokenDistributionProvider>
