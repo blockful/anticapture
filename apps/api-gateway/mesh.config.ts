a30f086865a3483102cc7bbc45adc212a6c53307
lucas picollo <lucaspicolloo@gmail.com>
2025-04-17 14:50:52 -0300

    feat: create api-gateway with graphql-mesh

---
+++ apps/api-gateway/mesh.config.ts
@@ -0,0 +1,16 @@
+import { createPrefixTransform, defineConfig, loadGraphQLHTTPSubgraph } from '@graphql-mesh/compose-cli'
+
+export const composeConfig = defineConfig({
+  subgraphs: [
+    {
+      sourceHandler: loadGraphQLHTTPSubgraph('Countries', {
+        endpoint: 'https://countries.trevorblades.com',
+      }),
+      transforms: [
+        createPrefixTransform({
+          value: "Countries_"
+        })
+      ]
+    }
+  ]
+})
