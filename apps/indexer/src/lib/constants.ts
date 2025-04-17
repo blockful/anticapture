93023a41e9c33d6acc64b858f3e27ef5e1ee7849
lucas picollo <lucaspicolloo@gmail.com>
2025-04-17 12:11:35 -0300

    chore: rename mainnet network to ethereum

--- apps/indexer/src/lib/constants.ts
+++ apps/indexer/src/lib/constants.ts
@@ -21,7 +21,7 @@ export const CONTRACT_ADDRESSES: Record<
     >
   >
 > = {
-  [NetworkEnum.MAINNET]: {
+  [NetworkEnum.ETHEREUM]: {
     [DaoIdEnum.UNI]: {
       token: {
         address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
