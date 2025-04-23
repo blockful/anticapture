a9b94315dd9f0d738e99536b753952700d4ffbec
lucas picollo <lucaspicolloo@gmail.com>
2025-04-23 16:09:54 -0300

    feat: generate supergraph from api envs

--- apps/api-gateway/mesh.config.ts
+++ apps/api-gateway/mesh.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig, loadGraphQLHTTPSubgraph } from '@graphql-mesh/compose-cli'
+import dotenv from "dotenv";
+import { defineConfig, loadGraphQLHTTPSubgraph, MeshComposeCLISubgraphConfig } from '@graphql-mesh/compose-cli'
+import { loadOpenAPISubgraph } from '@omnigraph/openapi'
 
-import { gatewayUrls } from './src/env'
+dotenv.config();
 
 export const composeConfig = defineConfig({
-  subgraphs: Object.entries(gatewayUrls).map(([key, url]) => ({
-    sourceHandler: loadGraphQLHTTPSubgraph(key, {
-      endpoint: url,
-    })
-  }))
+  subgraphs: [
+    ...Object.entries(process.env).reduce((acc, [key, value]) => {
+      const [match, network] = key.match(/^DAO_API_(\w+)/) || []
+      if (!match) return acc
+      return [...acc, {
+        sourceHandler: loadGraphQLHTTPSubgraph(network, {
+          endpoint: value!,
+        })
+      }]
+    }, [] as MeshComposeCLISubgraphConfig[]),
+    {
+      sourceHandler: loadOpenAPISubgraph("Petition", {
+        source: "https://dev.api.anticapture.com/doc-json",
+      }),
+    },
+  ]
 })
