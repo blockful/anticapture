c50178a363f5b77351542faab5d8ab4f252d7fed
lucas picollo <lucaspicolloo@gmail.com>
2025-04-24 13:54:03 -0300

    fix: read petition api from env

--- apps/api-gateway/mesh.config.ts
+++ apps/api-gateway/mesh.config.ts
@@ -4,21 +4,26 @@ import { loadOpenAPISubgraph } from '@omnigraph/openapi'
 
 dotenv.config();
 
+const subgraphs: MeshComposeCLISubgraphConfig[] = [
+  ...Object.entries(process.env).reduce((acc, [key, value]) => {
+    const [match, network] = key.match(/^DAO_API_(\w+)/) || []
+    if (!match) return acc
+    return [...acc, {
+      sourceHandler: loadGraphQLHTTPSubgraph(network, {
+        endpoint: value!,
+      })
+    }]
+  }, [] as MeshComposeCLISubgraphConfig[])
+]
+
+if (process.env.PETITION_API_URL) {
+  subgraphs.push({
+    sourceHandler: loadOpenAPISubgraph("Petition", {
+      source: process.env.PETITION_API_URL,
+    }),
+  })
+}
+
 export const composeConfig = defineConfig({
-  subgraphs: [
-    ...Object.entries(process.env).reduce((acc, [key, value]) => {
-      const [match, network] = key.match(/^DAO_API_(\w+)/) || []
-      if (!match) return acc
-      return [...acc, {
-        sourceHandler: loadGraphQLHTTPSubgraph(network, {
-          endpoint: value!,
-        })
-      }]
-    }, [] as MeshComposeCLISubgraphConfig[]),
-    {
-      sourceHandler: loadOpenAPISubgraph("Petition", {
-        source: "https://dev.api.anticapture.com/doc-json",
-      }),
-    },
-  ]
+  subgraphs
 })
