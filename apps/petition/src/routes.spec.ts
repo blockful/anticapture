9af129e8ce3243812aea100d24f1b56056d2dc98
lucas picollo <lucaspicolloo@gmail.com>
2025-05-02 12:00:57 -0300

    test: petition api test stucture

---
+++ apps/petition/src/routes.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import fastify from 'fastify';
+import { FastifyInstance } from "fastify";
+import { PetitionService } from "./services";
+
+
+describe("Petition Routes", () => {
+
+  let app: FastifyInstance;
+  let petitionService: PetitionService;
+
+  beforeEach(() => {
+    app = fastify();
+    // petitionService = new PetitionService(
+    //   mockDb,
+    //   mockAnticaptureClient
+    // );
+  })
+
+  describe("POST /petitions/:daoId", () => {
+    it("should sign a petition", () => {
+
+      const supportedDAOs = ["dao1", "dao2"];
+    });
+  })
+
+});
