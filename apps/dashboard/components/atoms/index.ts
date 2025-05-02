40033e0d664e19950146942d7796fee5c57ea253
eduramme <eduramme@gmail.com>
2025-05-02 16:14:30 -0300

    feat: create StagesCardRequirements components

--- apps/dashboard/components/atoms/index.ts
+++ apps/dashboard/components/atoms/index.ts
@@ -37,3 +37,4 @@ export * from "@/components/atoms/TokenDistributionCustomTooltip";
 export * from "@/components/atoms/TooltipInfo";
 export * from "@/components/atoms/ShowYourSupportStickyBar";
 export * from "@/components/atoms/CorneredBox";
+export { StagesCardRequirements } from "@/components/atoms/StagesCardRequirements";
