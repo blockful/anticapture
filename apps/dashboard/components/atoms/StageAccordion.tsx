d4bfed05007ac3006b9bf4ef6525df5c4bbdeb07
heronlancellot <heronlancellot@id.uff.br>
2025-05-06 10:03:46 -0300

    update: stage-accordion & content

--- apps/dashboard/components/atoms/StageAccordion.tsx
+++ apps/dashboard/components/atoms/StageAccordion.tsx
@@ -6,13 +6,13 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@radix-ui/react-accordion";
-import { AlertTriangle, CheckCircle, MinusIcon } from "lucide-react";
+import { MinusIcon } from "lucide-react";
 import { ReactNode } from "react";
 import { PlusIcon } from "@/components/atoms";
 import { Stage } from "@/components/atoms/StageTag";
 import { StageTagSimplified } from "@/components/atoms/StageTagSimplified";
 import { cn } from "@/lib/client/utils";
-import { DotFilledIcon } from "@radix-ui/react-icons";
+import { StageContent } from "@/components/atoms/StageContent";
 
 interface AccordionDataProps {
   subtitle: string;
@@ -25,124 +25,94 @@ const AccordionData: AccordionDataProps[] = [
     subtitle: "",
     title: <StageTagSimplified stage={Stage.ZERO} />,
     content: (
-      <div className="flex w-full flex-col gap-4 rounded-md bg-lightDark p-3 sm:flex-row">
-        <div className="flex flex-col gap-1 sm:max-w-[200px]">
-          <h3 className="font-roboto text-[13px] font-medium uppercase leading-[18px] tracking-[6%] text-white">
-            Critical Vulnerabilities
-          </h3>
-          <p className="text-sm font-normal text-foreground">
-            At least one High Risk issue identified
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h4 className="text-xs font-medium tracking-[6%] text-white">
-            REQUIREMENTS
-          </h4>
-          <div className="flex flex-row gap-2">
-            <div className="flex items-start">
-              <CheckCircle className="size-4 text-success" />
-            </div>
-            <p className="flex flex-wrap text-sm font-normal text-foreground">
-              All DAOs that have governor and timelock are considered at
-              least&nbsp;
-              <span className="whitespace-nowrap text-white">Stage 0.</span>
-              <span className="block">
-                At this stage, critical risks still be present and require
-                attention.
-              </span>
-            </p>
-          </div>
-        </div>
-      </div>
+      <StageContent
+        stage={Stage.ZERO}
+        title="Critical Vulnerabilities"
+        description="At least one High Risk issue identified"
+        type="requirements"
+        requirementText={
+          <>
+            <span className="block">
+              All DAOs that have governor and timelock are considered at least{" "}
+              <span className="whitespace-nowrap text-white">Stage 0</span>.
+            </span>
+            <span className="block">
+              At this stage, critical risks still be present and require
+              attention.
+            </span>
+          </>
+        }
+      />
     ),
   },
   {
-    subtitle: "",
-    title: <StageTagSimplified stage={Stage.ONE} />,
+    subtitle: "2 issues needs fixing",
+    title: <StageTagSimplified stage={Stage.ONE} isCompleted={false} />,
     content: (
-      <div className="flex w-full flex-col gap-4 rounded-md bg-lightDark p-3 sm:flex-row">
-        <div className="flex flex-col gap-1 sm:max-w-[200px]">
-          <h3 className="font-roboto text-[13px] font-medium uppercase leading-[18px] tracking-[6%] text-white">
-            Partial Risk Reduction
-          </h3>
-          <p className="text-sm font-normal text-foreground">
-            No High Risk issues, at least one Medium Risk issue remains
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h4 className="text-xs font-medium tracking-[6%] text-white">
-            REQUIREMENTS
-          </h4>
-          <div className="flex flex-row gap-2">
-            <div className="flex items-start">
-              <CheckCircle className="size-4 text-success" />
-            </div>
-            <p className="flex flex-wrap text-sm font-normal text-foreground">
-              Has no implementation details flagged as High Risk, but at least
-              one remains at Medium Risk, qualifying it as&nbsp;
-              <span className="whitespace-nowrap text-white"> Stage 1.</span>
-              <span className="block">
-                Progress has been made, but further improvements are still
-                needed to enhance security.
-              </span>
-            </p>
-          </div>
-        </div>
-      </div>
+      <StageContent
+        stage={Stage.ONE}
+        title="Partial Risk Reduction"
+        description="No High Risk issues, at least one Medium Risk issue remains"
+        type="issues"
+        issues={[
+          {
+            title: "Voting Period",
+            description: [
+              "The current voting period of 2 days is too short to allow for aligned delegates to defend the DAO against an attack.",
+              "To change this the DAO needs to approve a proposal to change this configuration in the governor contract.",
+              "To be qualified as low risk, the Voting Period should be of 7 days or more.",
+            ],
+          },
+          {
+            title: "Spam Prevention",
+            description: [
+              "The DAO currently lacks sufficient protections against proposal spamming, increasing the risk of governance attacks.",
+            ],
+          },
+          {
+            title: "Voting Subsidy",
+            description: [
+              "Without support mechanisms, active participation in governance can decline due to high transaction costs.",
+              "To change this, the DAO can implement a voting subsidy to reimburse or reward users who vote on proposals.",
+            ],
+          },
+        ]}
+      />
     ),
   },
   {
     subtitle: "3 issue needs fixing",
     title: <StageTagSimplified stage={Stage.TWO} isCompleted={false} />,
     content: (
-      <div className="flex w-full flex-col gap-4 rounded-md bg-lightDark p-3 sm:flex-row">
-        <div className="flex flex-col gap-1 sm:max-w-[200px]">
-          <h3 className="font-roboto text-[13px] font-medium uppercase leading-[18px] tracking-[6%] text-white">
-            Minimal Risks
-          </h3>
-          <p className="text-sm font-normal text-foreground">
-            No High Risk issues, at least one Medium Risk issue remains
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h4 className="text-xs font-medium uppercase tracking-[6%] text-white">
-            issues that need to be fixed
-          </h4>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-start gap-2">
-              <AlertTriangle className="size-4 text-warning" />
-              <p className="text-sm font-normal text-warning">Voting Period</p>
-            </div>
-            <div className="flex justify-start gap-2">
-              <div>
-                <DotFilledIcon className="size-4 text-[#52525B]" />
-              </div>
-              <p className="text-sm font-normal text-foreground">
-                The current voting period of 2 days is too short to allow for
-                aligned delegates to defend the DAO against an attack.
-              </p>
-            </div>
-          </div>
-          <div className="border-t border-middleDark" />
-          <div className="flex flex-col gap-2">
-            <div className="flex items-start gap-2">
-              <AlertTriangle className="size-4 text-warning" />
-              <p className="text-sm font-normal text-warning">
-                Spam Prevention
-              </p>
-            </div>
-            <div className="flex justify-start gap-2">
-              <div>
-                <DotFilledIcon className="size-4 text-[#52525B]" />
-              </div>
-              <p className="text-sm font-normal text-foreground">
-                The DAO currently lacks sufficient protections against proposal
-                spamming, increasing the risk of governance attacks.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <StageContent
+        stage={Stage.TWO}
+        title="Minimal Risks"
+        description="No High Risk issues, at least one Medium Risk issue remains"
+        type="issues"
+        issues={[
+          {
+            title: "Voting Period",
+            description: [
+              "The current voting period of 2 days is too short to allow for aligned delegates to defend the DAO against an attack.",
+              "To change this the DAO needs to approve a proposal to change this configuration in the governor contract.",
+              "To be qualified as low risk, the Voting Period should be of 7 days or more.",
+            ],
+          },
+          {
+            title: "Spam Prevention",
+            description: [
+              "The DAO currently lacks sufficient protections against proposal spamming, increasing the risk of governance attacks.",
+            ],
+          },
+          {
+            title: "Voting Subsidy",
+            description: [
+              "Without support mechanisms, active participation in governance can decline due to high transaction costs.",
+              "To change this, the DAO can implement a voting subsidy to reimburse or reward users who vote on proposals.",
+            ],
+          },
+        ]}
+      />
     ),
   },
 ];
---
+++ apps/dashboard/components/atoms/StageContent.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { ReactNode } from "react";
+import { AlertTriangle, CheckCircle } from "lucide-react";
+import { DotFilledIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/client/utils";
+import { Stage } from "@/components/atoms/StageTag";
+
+interface Issue {
+  title: string;
+  description: string[];
+}
+
+interface StageContentProps {
+  stage?: Stage;
+  title: string;
+  description: string;
+  type: "requirements" | "issues";
+  issues?: Issue[];
+  requirementText?: ReactNode;
+}
+
+export const StageContent = ({
+  stage,
+  title,
+  description,
+  type,
+  issues,
+  requirementText,
+}: StageContentProps) => {
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <div className="flex w-full flex-col gap-4 rounded-md bg-lightDark p-3 sm:flex-row">
+        <div className="flex flex-col gap-1 sm:max-w-[200px]">
+          <h3 className="font-roboto text-[13px] font-medium uppercase leading-[18px] tracking-wide text-white">
+            {title}
+          </h3>
+          <p className="text-sm font-normal text-foreground">{description}</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          {type === "requirements" && (
+            <h4 className="font-roboto text-xs font-medium uppercase tracking-wide text-white">
+              Requirements
+            </h4>
+          )}
+          {type === "issues" && (
+            <h4
+              className={cn(
+                "font-roboto text-xs font-medium uppercase tracking-wide text-white",
+                stage === Stage.ONE && "text-error",
+                stage === Stage.TWO && "text-warning",
+              )}
+            >
+              Issues that need to be fixed
+            </h4>
+          )}
+
+          {type === "requirements" ? (
+            <div className="flex flex-row gap-2">
+              <div className="flex items-start">
+                <CheckCircle className="size-4 text-success" />
+              </div>
+              <p className="flex flex-wrap text-sm font-normal text-foreground">
+                {requirementText}
+              </p>
+            </div>
+          ) : (
+            issues?.map((issue, index) => (
+              <div key={index}>
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-start gap-2">
+                    <AlertTriangle
+                      className={cn(
+                        "size-4",
+                        stage === Stage.ONE && "text-error",
+                        stage === Stage.TWO && "text-warning",
+                      )}
+                    />
+                    <p className="text-sm font-normal text-white">
+                      {issue.title}
+                    </p>
+                  </div>
+                  <div className="flex justify-start gap-2">
+                    <div className="flex flex-col gap-1">
+                      {issue.description.map((desc, i) => (
+                        <div key={i} className="flex flex-row gap-2">
+                          <div>
+                            <DotFilledIcon className="size-4 text-[#52525B]" />
+                          </div>
+                          <p
+                            key={i}
+                            className="text-sm font-normal text-foreground"
+                          >
+                            {desc}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                {index < issues.length - 1 && (
+                  <div className="my-2 border-t border-middleDark" />
+                )}
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
