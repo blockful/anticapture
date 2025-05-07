b2a03b403b58f0042a6eb76eeceb41c16c5369fb
heronlancellot <heronlancellot@id.uff.br>
2025-05-07 09:42:56 -0300

    fix: border-none tablet or more

--- apps/dashboard/components/ui/table.tsx
+++ apps/dashboard/components/ui/table.tsx
@@ -66,7 +66,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "hover:bg-accent data-[state=selected]:bg-muted border-b transition-colors [&_td:first-child]:border-r [&_td:first-child]:border-white/10",
+      "hover:bg-accent data-[state=selected]:bg-muted border-b transition-colors [&_td:first-child]:border-r [&_td:first-child]:border-white/10 md:[&_td:first-child]:border-none",
       // "[&_td:first-child]:sticky [&_td:first-child]:left-0 [&_td:first-child]:z-10",
       // "[&_td:first-child]:shadow-[2px_0px_8px_2px_rgba(0,0,0,1.00)] sm:[&_td:first-child]:shadow-none",
       className,
