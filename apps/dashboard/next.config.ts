cf63135b8dea39717c12ca9aeac67ef46360e1b6
heronlancellot <heronlancellot@id.uff.br>
2025-05-21 09:56:18 -0300

    fix: remove turbo-pack

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -20,6 +20,19 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
