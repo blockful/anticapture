132024983b4e1b1858677e887651f02bc31ed4e1
heronlancellot <heronlancellot@id.uff.br>
2025-05-21 11:51:50 -0300

    fix: text-opacity

--- apps/dashboard/features/support-dao/components/ReachOutToUsCard.tsx
+++ apps/dashboard/features/support-dao/components/ReachOutToUsCard.tsx
@@ -6,7 +6,7 @@ import { Card } from "@/shared/components/ui/card";
 export const ReachOutToUsCard = () => {
   return (
     <Card
-      className="flex w-full rounded-lg border border-light-dark bg-dark px-3 py-3 shadow-sm hover:cursor-pointer hover:bg-tangerine/15 sm:w-[calc(50%-10px)] md:px-4 xl4k:max-w-full"
+      className="border-light-dark bg-dark hover:bg-tangerine/15 xl4k:max-w-full flex w-full rounded-lg border px-3 py-3 shadow-sm hover:cursor-pointer sm:w-[calc(50%-10px)] md:px-4"
       onClick={() => {
         window.open(
           "https://tally.so/r/nrvGbv",
@@ -18,8 +18,8 @@ export const ReachOutToUsCard = () => {
       <div className="flex w-full justify-between">
         <div className="flex flex-row items-center">
           <div className="flex items-center gap-2">
-            <div className="flex size-9 items-center justify-center rounded-full border border-middle-dark bg-light-dark sm:size-6">
-              <Globe className="size-4 text-foreground text-opacity-70" />
+            <div className="border-middle-dark bg-light-dark flex size-9 items-center justify-center rounded-full border sm:size-6">
+              <Globe className="text-foreground/70 size-4" />
             </div>
             <div className="flex flex-col justify-start lg:flex-row lg:items-center lg:gap-1.5">
               <h3 className="text-sm font-medium text-white">
@@ -27,7 +27,7 @@ export const ReachOutToUsCard = () => {
               </h3>
               <div className="flex h-full items-center gap-1">
                 <h3 className="link-tangerine uppercase">Reach out to us</h3>
-                <ArrowRightIcon className="size-4 text-tangerine" />
+                <ArrowRightIcon className="text-tangerine size-4" />
               </div>
             </div>
           </div>
