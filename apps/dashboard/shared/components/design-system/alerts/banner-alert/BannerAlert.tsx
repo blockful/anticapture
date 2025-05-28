b72ac0fa70a72fcefa3796d63543c32c0301add2
eduramme <eduramme@gmail.com>
2025-05-28 18:26:07 -0300

    feat: update alert font to roboto mono

--- apps/dashboard/shared/components/design-system/alerts/banner-alert/BannerAlert.tsx
+++ apps/dashboard/shared/components/design-system/alerts/banner-alert/BannerAlert.tsx
@@ -53,7 +53,9 @@ const BannerAlert = ({
       <div className="flex items-center gap-2 tracking-wider sm:flex-row">
         {icon}
         <div className="flex flex-col items-center gap-1 sm:flex-row">
-          <div className="flex gap-3 text-xs text-white uppercase">{text}</div>
+          <div className="flex gap-3 font-mono text-xs text-white uppercase">
+            {text}
+          </div>
           {link && (
             <DefaultLink href={link.url} openInNewTab variant="highlight">
               {link.text}
