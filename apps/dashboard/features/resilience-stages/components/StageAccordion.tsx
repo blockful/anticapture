37b48ca76a1feb9376ec47713066c04ea423655e
heronlancellot <heronlancellot@id.uff.br>
2025-06-04 18:14:16 -0300

    update: to use the formatPlural

--- apps/dashboard/features/resilience-stages/components/StageAccordion.tsx
+++ apps/dashboard/features/resilience-stages/components/StageAccordion.tsx
@@ -11,6 +11,7 @@ import { ReactNode } from "react";
 import { Stage } from "@/shared/types/enums/Stage";
 import { StageTagSimplified } from "@/shared/components/tags/StageTagSimplified";
 import { cn } from "@/shared/utils/";
+import { formatPlural } from "@/shared/utils/formatPlural";
 import { StageContent } from "@/features/resilience-stages/components/StageContent";
 import { GovernanceImplementationField } from "@/shared/dao-config/types";
 import { RiskLevel } from "@/shared/types/enums";
@@ -129,9 +130,7 @@ const CustomAccordionItem = ({
               <div className="flex flex-row items-center gap-2">
                 <div className="bg-middle-dark size-1 rounded-full" />
                 <p className="text-sm font-normal text-white">
-                  {`${riskFields.length} issue${
-                    riskFields.length > 1 ? "s" : ""
-                  } needs fixing`}
+                  {`${formatPlural(riskFields.length, "issue")} needs fixing`}
                 </p>
               </div>
             )}
