b0ed3a4f4c1a49013f0cafa050ec62f261c5f2d7
heronlancellot <heronlancellot@id.uff.br>
2025-06-09 10:57:17 -0300

    update: text-alternative xs

--- apps/dashboard/shared/components/cards/BaseCardDaoInfo.tsx
+++ apps/dashboard/shared/components/cards/BaseCardDaoInfo.tsx
@@ -37,7 +37,7 @@ export const BaseCardDaoInfo = ({ data }: BaseCardDaoInfoProps) => {
             data.optionalHeaderValue ? "justify-between" : "justify-start",
           )}
         >
-          <CardTitle className="text-primary flex items-center gap-2 text-xs font-semibold uppercase">
+          <CardTitle className="!text-alternative-xs text-primary flex items-center gap-2 font-mono font-semibold uppercase">
             {data.icon}
             {data.title}
           </CardTitle>
