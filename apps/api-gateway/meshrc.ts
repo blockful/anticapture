4734cc0d199db66df647ca17626c58dac3879edb
lucas picollo <lucaspicolloo@gmail.com>
2025-06-10 11:22:57 -0300

    refactor: gateway link resources through envs

--- apps/api-gateway/meshrc.ts
+++ apps/api-gateway/meshrc.ts
@@ -1,56 +1,35 @@
 import dotenv from "dotenv";
-import { Resource } from "sst";
 
 import { processConfig } from '@graphql-mesh/config'
 
 dotenv.config();
 
-console.log({ Resource })
-
-const env =
-  process.env.NODE_ENV === "local"
-    ? process.env
-    : {
-      ...Object.entries(Resource)
-        .filter(([key, value]) => /(\w+)IndexerAPI/.test(key) && value.url)
-        .map(([key, value]) => {
-          const daoName = /(\w+)IndexerAPI/.exec(key)?.[1];
-          return [`DAO_API_${daoName?.toUpperCase()}`, value.url];
-        })
-        .reduce((acc, [key, value]) => ({
-          ...acc,
-          [key]: value
-        }), {}),
-    }
-
 export default processConfig({
-  sources: [
-    ...Object.entries(env)
-      .filter(([key]) => key.startsWith('DAO_API_'))
-      .flatMap(([key, value]) => {
-        if (!value) return [];
+  sources: Object.entries(process.env)
+    .filter(([key]) => key.startsWith('DAO_API_'))
+    .flatMap(([key, value]) => {
+      if (!value) return [];
 
-        const daoName = key.replace('DAO_API_', '');
-        return [
-          {
-            name: `graphql_${daoName}`,
-            handler: {
-              graphql: {
-                endpoint: value,
-              }
-            }
-          },
-          {
-            name: `rest_${daoName}`,
-            handler: {
-              openapi: {
-                source: `${value}/docs`,
-              }
+      const daoName = key.replace('DAO_API_', '');
+      return [
+        {
+          name: `graphql_${daoName}`,
+          handler: {
+            graphql: {
+              endpoint: value,
             }
           }
-        ];
-      }),
-  ],
+        },
+        {
+          name: `rest_${daoName}`,
+          handler: {
+            openapi: {
+              source: `${value}/docs`,
+            }
+          }
+        }
+      ];
+    }),
   additionalResolvers: [
     "src/resolvers/index",
   ]
--- apps/infra/src/gateway.ts
+++ apps/infra/src/gateway.ts
@@ -1,14 +1,23 @@
 /// <reference path="../.sst/platform/config.d.ts" />
 
-export function newGateway(cluster: sst.aws.Cluster, services: sst.aws.Service[]): sst.aws.Service {
+export function newGateway(cluster: sst.aws.Cluster, services: { service: sst.aws.Service, dao: string }[]): sst.aws.Service {
+  // console.log({ services })
+
+  // const parsedServices = services.map(({ service, dao }) => ({
+  //   [`DAO_API_${dao.toUpperCase()}`]: $interpolate`${service.url}`,
+  // }))
+
+  // console.log({ parsedServices })
+
   return new sst.aws.Service("APIGateway", {
     cluster,
     memory: "0.5 GB",
     cpu: "0.25 vCPU",
-    link: services,
+    link: services.map(({ service }) => service),
     environment: {
       NODE_ENV: $dev ? "development" : "production",
-      DEBUG: 1
+      DEBUG: 1,
+      // ...parsedServices
     },
     image: {
       context: "../..",
