c7eb889b8478d3c12c09a9d22ad68c1d75638493
Alexandro T. Netto <alex.t.netto@gmail.com>
2025-07-05 18:28:26 +0100

    chore: decouple config params

--- apps/indexer/config/uniswap.config.ts
+++ apps/indexer/config/uniswap.config.ts
@@ -1,10 +1,11 @@
 import { createConfig } from "ponder";
-import { http } from "viem";
 import { CONTRACT_ADDRESSES } from "@/lib/constants";
 import { DaoIdEnum, NetworkEnum } from "@/lib/enums";
 
 import { env } from "@/env";
 import { UNIGovernorAbi, UNITokenAbi } from "@/indexer/uni/abi";
+
+const UNI_CONTRACTS = CONTRACT_ADDRESSES[NetworkEnum.ETHEREUM][DaoIdEnum.UNI]!;
 
 export default createConfig({
   database: {
@@ -23,16 +24,14 @@ export default createConfig({
     UNIToken: {
       abi: UNITokenAbi,
       chain: "ethereum_mainnet",
-      address:
-        CONTRACT_ADDRESSES[NetworkEnum.ETHEREUM][DaoIdEnum.UNI]!.token.address,
-      startBlock: 10861674,
+      address: UNI_CONTRACTS.token.address,
+      startBlock: UNI_CONTRACTS.token.startBlock,
     },
     UNIGovernor: {
       abi: UNIGovernorAbi,
       chain: "ethereum_mainnet",
-      address:
-        CONTRACT_ADDRESSES[NetworkEnum.ETHEREUM][DaoIdEnum.UNI]!.governor,
-      startBlock: 13059157,
+      address: UNI_CONTRACTS.governor!.address,
+      startBlock: UNI_CONTRACTS.governor!.startBlock,
     },
   },
 });
