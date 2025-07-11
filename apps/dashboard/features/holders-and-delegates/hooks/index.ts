7290c8d855abfa1ce394f264ba3321a948ac5d04
heronlancellot <heronlancellot@id.uff.br>
2025-07-11 16:16:33 -0300

    fix: remove use-account-balance-mappings

--- apps/dashboard/features/holders-and-delegates/hooks/index.ts
+++ apps/dashboard/features/holders-and-delegates/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useDelegationHistory } from "./useDelegationHistory";
 export { useTokenHolders } from "./useTokenHolders";
 export { useDelegates } from "./useDelegates";
-export { useAccountBalanceMapping } from "./useAccountBalanceMapping";
