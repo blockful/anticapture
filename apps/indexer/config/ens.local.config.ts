860da781d989866b620d279dc29a9ab837ca2e64
lucas picollo <lucaspicolloo@gmail.com>
2025-07-15 14:50:14 -0300

    chore: ens local config with hardcoded contracts

--- apps/indexer/config/ens.local.config.ts
+++ apps/indexer/config/ens.local.config.ts
@@ -3,18 +3,6 @@ import { createConfig } from "ponder";
 import { env } from "@/env";
 import { ENSGovernorAbi, ENSTokenAbi } from "@/indexer/ens/abi";
 
-const CONTRACTS = {
-  token: {
-    address: "0x244dE6b06E7087110b94Cde88A42d9aBA17efa52",
-    decimals: 18,
-    startBlock: 22635098,
-  },
-  governor: {
-    address: "0x7c28FC9709650D49c8d0aED2f6ece6b191F192a9",
-    startBlock: 22635098,
-  },
-} as const;
-
 export default createConfig({
   database: {
     kind: "postgres",
@@ -24,8 +12,6 @@ export default createConfig({
     anvil: {
       id: 31337,
       rpc: env.RPC_URL,
-      maxRequestsPerSecond: 10,
-      pollingInterval: 1000,
     },
   },
   // NOTE: These addresses are deterministic for Anvil local development
@@ -36,14 +22,14 @@ export default createConfig({
     ENSToken: {
       abi: ENSTokenAbi,
       chain: "anvil",
-      address: CONTRACTS.token.address,
-      startBlock: CONTRACTS.token.startBlock, // Block where ENS Token was deployed
+      address: "0x244dE6b06E7087110b94Cde88A42d9aBA17efa52",
+      startBlock: 22635098,
     },
     ENSGovernor: {
       abi: ENSGovernorAbi,
       chain: "anvil",
-      address: CONTRACTS.governor.address,
-      startBlock: CONTRACTS.governor.startBlock, // Block where ENS Governor was deployed
+      address: "0x7c28FC9709650D49c8d0aED2f6ece6b191F192a9",
+      startBlock: 22635098,
     },
   },
 });
