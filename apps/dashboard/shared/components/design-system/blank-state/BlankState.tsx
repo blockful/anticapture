30e5c7c932ad8ae442d7eac3ffb44a54c725a817
Luiz Gustavo Abou Hatem de Liz <luizgustavoashc@gmail.com>
2025-07-18 19:40:44 -0300

    fix: fix build;

--- apps/dashboard/shared/components/design-system/blank-state/BlankState.tsx
+++ apps/dashboard/shared/components/design-system/blank-state/BlankState.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ElementType } from "react";
+import { ElementType, ReactNode } from "react";
 import { cva } from "class-variance-authority";
 import { cn } from "@/shared/utils/cn";
 
