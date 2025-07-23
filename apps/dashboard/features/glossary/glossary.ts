665facb800199e222555e69473835c04f21d253d
eduramme <eduramme@gmail.com>
2025-07-23 15:11:06 -0300

    feat: add other real words to glossary

--- apps/dashboard/features/glossary/glossary.ts
+++ apps/dashboard/features/glossary/glossary.ts
@@ -152,22 +152,194 @@ export const SAMPLE_GLOSSARY_DATA: GlossaryData = {
         "Cost of opening a long/short on a token. The cost comes from borrowing money from the exchange in order to leverage itself in the token.",
     },
   ],
-  G: [],
-  H: [],
-  I: [],
+  G: [
+    {
+      term: "Gini Coefficient",
+      definition:
+        "A metric that measures the inequality of a system. In the context of DAOs, it can be applied to analyze how centralized a governance system is.",
+    },
+    {
+      term: "Governance tokens",
+      definition:
+        "Tokens used to participate in the decisions of a decentralized project.",
+    },
+    {
+      term: "Governor",
+      definition:
+        "Type of structure for governance contracts, present in DAOs such as Uniswap and Compound.",
+    },
+  ],
+  H: [
+    {
+      term: "Humpy",
+      definition:
+        "A figure known for attacking DAOs such as Balancer, Aura, Compound, and Sushi.",
+    },
+  ],
+  I: [
+    {
+      term: "Incident Response",
+      definition:
+        "Type of report done for cybersecurity attacks. In the context of DAOs, it will be used as a response to governance attacks.",
+    },
+  ],
   J: [],
   K: [],
-  L: [],
-  M: [],
-  N: [],
-  O: [],
-  P: [],
-  Q: [],
-  R: [],
-  S: [],
-  T: [],
+  L: [
+    {
+      term: "L2 Beat",
+      definition:
+        "The company responsible for creating the security standard for second-layer blockchains in the market. Used as a reference to develop Anticapture and our framework.",
+    },
+    {
+      term: "Lending",
+      definition:
+        "Projects that offer markets for users to provide liquidity for others to lend funds, putting up collateral to guarantee their loans.",
+    },
+    {
+      term: "Leverage",
+      definition:
+        "A strategy where you borrow funds from a third party to increase your exposure to an asset. Exchanges/applications allow investors to leverage themselves on tokens, to bet for (long) or against (short) their performance. In the context of DAOs, it is useful for an attacker to attack a DAO and bet against its governance token, profiting from its devaluation.",
+    },
+    {
+      term: "Liquid Treasury",
+      definition:
+        "The assets in the treasury that have market liquidity to be sold, not counting the organization's own governance tokens.",
+    },
+    {
+      term: "Liquidity",
+      definition:
+        "The amount of money available on the market to absorb the purchase/sale of a token.",
+    },
+    {
+      term: "Liquidity Mining",
+      definition:
+        "Reward/incentive programs carried out by projects, usually with governance tokens.",
+    },
+    {
+      term: "LP",
+      definition:
+        "Representation of participation in the liquidity of a pool in a protocol.",
+    },
+  ],
+  M: [
+    {
+      term: "Market cap",
+      definition:
+        "Value of all the tokens in circulation in a project. Equals the current value of the token, multiplied by the number of tokens in circulation.",
+    },
+    {
+      term: "Max Supply",
+      definition: "Maximum number of tokens that will be issued by a project.",
+    },
+    {
+      term: "Metaprotocol",
+      definition:
+        "A project whose economy is built on top of another protocol, such as Convex for Curve or Aura for Balancer.",
+    },
+  ],
+  N: [
+    {
+      term: "Nakamoto Coefficient",
+      definition:
+        "A metric that measures how decentralized a network is. Created to measure the decentralization of Bitcoin, but can also be applied in the context of DAOs.",
+    },
+  ],
+  O: [
+    {
+      term: "Open Interest",
+      definition:
+        "The total value of open longs and shorts on a token. Measures the capital willing to bet for or against a token.",
+    },
+  ],
+  P: [
+    {
+      term: "Proposal Threshold",
+      definition:
+        "Number of governance tokens (votes) required to submit a proposal in a DAO.",
+    },
+  ],
+  Q: [
+    {
+      term: "Quorum",
+      definition:
+        "The number of votes needed for a proposal to be considered for approval in governance.",
+    },
+  ],
+  R: [
+    {
+      term: "RFV",
+      definition:
+        "Risk Free Value indicates that an organization has more assets under management than the total cost of their tokens. Identified through an RFV Coefficient of 1 or higher.",
+    },
+    {
+      term: "RFV Coefficient",
+      definition:
+        "RFVC, a metric that measures how much a token total supply is worth in relation to the liquid assets in the treasury of the project that issues it. Calculated as Liquid Treasury divided by Market Cap",
+    },
+    {
+      term: "RFV Raiders",
+      definition:
+        "Group specializing in finding governance tokens that are underpriced in relation to the treasuries of their respective DAOs.",
+    },
+  ],
+  S: [
+    {
+      term: "Security Council",
+      definition:
+        "A mechanism designed to protect DAOs from attacks. It brings together people with a reputation in the organization, a multisig, and the ability to cancel proposals. There are standards developed by L2 Beat and 1kx, followed by Blockful.",
+    },
+    {
+      term: "Slow Rug",
+      definition:
+        "An attack in which DAO members gradually extract resources from the organization by approving proposals that are supposed to benefit the DAO, but only benefit a select group of people.",
+    },
+    {
+      term: "Stages",
+      definition:
+        "Logic of security stages, created by L2 Beat, adopted by Blockful for DAOs. Certain requirements must be met in order to fall within certain security stages. The more requirements met, the higher the stage - and the security - of a project.",
+    },
+  ],
+  T: [
+    {
+      term: "Temperature Check",
+      definition:
+        "Preliminary voting, carried out using tools such as Snapshot, to gauge community sentiment towards a proposal. It's usually a step that precedes the on-chain vote.",
+    },
+    {
+      term: "Timelock",
+      definition:
+        'The waiting time for a proposal/vote to be executed. Programmed in the governance contract of a DAO. Usually exists as its own smart contract, "Timelock Contract".',
+    },
+    {
+      term: "Treasury",
+      definition:
+        "Funds of a decentralized organization. It is usually made up of governance tokens and tokens such as stablecoins, ETH and BTC.",
+    },
+  ],
   U: [],
-  V: [],
+  V: [
+    {
+      term: "veNomics",
+      definition:
+        "A type of economy where a token only gains governance power after being locked into a contract for a set period of time. It is also common to have a system for voting on token issues, where the holders of the governance tokens define where the emissions made by the DAO will go.",
+    },
+    {
+      term: "Voting Delay",
+      definition:
+        "The period between the end of the submission of the proposal and the start of the vote (Voting Period).",
+    },
+    {
+      term: "Voting Period",
+      definition:
+        "The period in which DAO members have to vote on a proposal in their governance.",
+    },
+    {
+      term: "Voting Power",
+      definition:
+        "Voting power of an address. This can be equivalent to the governance tokens stored by the address or the number of delegations received by the address.",
+    },
+  ],
   W: [],
   X: [],
   Y: [],
