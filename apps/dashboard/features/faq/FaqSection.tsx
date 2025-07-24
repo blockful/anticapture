258e7eab1a3ae717b9607ab1cdc9c85901ebe4f2
Eduardo <eduramme@gmail.com>
2025-07-24 15:59:15 -0300

    Update apps/dashboard/features/faq/FaqSection.tsx
    Co-authored-by: heronlancellot.eth <heronlancellot@gmail.com>

--- apps/dashboard/features/faq/FaqSection.tsx
+++ apps/dashboard/features/faq/FaqSection.tsx
@@ -93,7 +93,7 @@ export const FaqSection = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="border-light-dark bg-surface-contrast text-primary hover:bg-surface-contrast/80 flex items-center gap-1.5 rounded-md border p-2 text-sm font-normal">
+            <button className="border-light-dark bg-surface-contrast text-primary hover:bg-surface-contrast/80 flex items-center gap-1.5 rounded-md border p-2 text-sm font-normal cursor-pointer">
               <TelegramIcon className="size-5" />
               Send Message
             </button>
