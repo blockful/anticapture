b80d13017d7cc5e0083045fcd806bad8665c8b22
eduramme <eduramme@gmail.com>
2025-08-07 20:57:56 -0300

    feat: make attack cost bar chart wattermark go up

--- apps/dashboard/features/attack-profitability/components/AttackCostBarChart.tsx
+++ apps/dashboard/features/attack-profitability/components/AttackCostBarChart.tsx
@@ -293,7 +293,7 @@ export const AttackCostBarChart = ({ className }: AttackCostBarChartProps) => {
               ))}
         </BarChart>
       </ResponsiveContainer>
-      <AnticaptureWatermark />
+      <AnticaptureWatermark svgClassName="pb-10" />
     </div>
   );
 };
