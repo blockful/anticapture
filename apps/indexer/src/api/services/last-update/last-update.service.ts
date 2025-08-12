84a333bd268ccdd4d0a36c2bb39566a4ba9c4968
Luiz Gustavo Abou Hatem de Liz <50002794+lgahdl@users.noreply.github.com>
2025-08-12 15:38:09 -0300

    Update apps/indexer/src/api/services/last-update/last-update.service.ts
    Co-authored-by: lucas picollo <lucaspicolloo@gmail.com>

--- apps/indexer/src/api/services/last-update/last-update.service.ts
+++ apps/indexer/src/api/services/last-update/last-update.service.ts
@@ -1,9 +1,10 @@
 import { ChartType } from "@/api/mappers/last-update";
-import { LastUpdateRepository } from "@/api/repositories/last-update.repository";
+interface LastUpdateRepository {
+  getLastUpdate(chart: ChartType): Promise<number | undefined>
+}
 
 export class LastUpdateService {
   constructor(private readonly repository: LastUpdateRepository) {}
-
   async getLastUpdate(chart: ChartType): Promise<string> {
     const lastUpdateDate = await this.repository.getLastUpdate(chart);
 
