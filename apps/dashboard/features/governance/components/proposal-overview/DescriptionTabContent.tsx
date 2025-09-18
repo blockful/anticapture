f82f9568524e602046bd5ecc8fdb10f00b4b8e05
eduramme <eduramme@gmail.com>
2025-09-18 08:35:13 -0300

    feat: include markdow to jsx

--- apps/dashboard/features/governance/components/proposal-overview/DescriptionTabContent.tsx
+++ apps/dashboard/features/governance/components/proposal-overview/DescriptionTabContent.tsx
@@ -1,4 +1,5 @@
 import { GetProposalQuery } from "@anticapture/graphql-client";
+import Markdown from "markdown-to-jsx";
 
 interface DescriptionTabContentProps {
   proposal: NonNullable<GetProposalQuery["proposal"]>;
@@ -7,5 +8,42 @@ interface DescriptionTabContentProps {
 export const DescriptionTabContent = ({
   proposal,
 }: DescriptionTabContentProps) => {
-  return <div className="text-primary p-4">{proposal.description}</div>;
+  return (
+    <div className="text-primary p-4">
+      <Markdown
+        options={{
+          overrides: {
+            h1: {
+              component: "p",
+              props: {
+                className: "text-primary ",
+              },
+            },
+            h2: {
+              component: "h2",
+              props: {
+                className: "text-primary mt-7 mb-4 text-[16px] leading-[24px]",
+              },
+            },
+            h3: {
+              component: "h3",
+              props: {
+                className: "text-primary mt-7 mb-4 text-[16px] leading-[24px]",
+              },
+            },
+            p: {
+              component: "p",
+              props: {
+                className: "text-secondary text-[14px] leading-[20px]",
+              },
+            },
+          },
+        }}
+      >
+        {proposal.description}
+      </Markdown>
+
+      {proposal.description}
+    </div>
+  );
 };
