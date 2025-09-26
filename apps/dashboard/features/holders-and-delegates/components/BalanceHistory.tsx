d0af3b777f11509a01c1f14f55eb2a5739303d06
heronlancellot.eth <heronlancellot@gmail.com>
2025-09-25 22:00:24 -0300

    Update apps/dashboard/features/holders-and-delegates/components/BalanceHistory.tsx
    Co-authored-by: Eduardo <eduramme@gmail.com>

--- apps/dashboard/features/holders-and-delegates/components/BalanceHistory.tsx
+++ apps/dashboard/features/holders-and-delegates/components/BalanceHistory.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useMemo, useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { TheTable, SkeletonRow, Button, IconButton } from "@/shared/components";
