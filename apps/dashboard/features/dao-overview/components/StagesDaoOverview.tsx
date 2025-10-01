6399f8e2cf51937af7404dfd0f56bd220bfb2780
eduramme <eduramme@gmail.com>
2025-10-01 18:07:45 -0300

    fix: dao overview fix

--- apps/dashboard/features/dao-overview/components/StagesDaoOverview.tsx
+++ apps/dashboard/features/dao-overview/components/StagesDaoOverview.tsx
@@ -101,7 +101,7 @@ export const StagesDaoOverview = ({
           <div className="flex justify-start">
             <Button
               variant="ghost"
-              className="group font-mono"
+              className="group px-0 py-0 font-mono md:px-4 md:py-2"
               onClick={handleButtonClick}
               onMouseEnter={() => !isMobile && setShowTooltip(true)}
             >
