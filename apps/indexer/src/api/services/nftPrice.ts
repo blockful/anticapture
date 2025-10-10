cc69eaa225037c8e6bb4b98af98e1ab45df71fbb
lucas picollo <lucaspicolloo@gmail.com>
2025-10-10 16:54:20 -0300

    fix: historical nft price as usd

--- apps/indexer/src/api/services/nftPrice.ts
+++ apps/indexer/src/api/services/nftPrice.ts
@@ -1,3 +1,6 @@
+import { formatEther } from "viem";
+import axios, { AxiosInstance } from "axios";
+
 import { TokenHistoricalPriceResponse } from "@/api/mappers";
 
 interface Repository {
@@ -9,13 +12,50 @@ interface Repository {
 }
 
 export class NFTPriceService {
-  constructor(private readonly repo: Repository) {}
+  private readonly coingeckoApiUrl = "https://api.coingecko.com/api/v3";
+  private readonly client: AxiosInstance;
+
+  constructor(
+    private readonly repo: Repository,
+    private readonly coingeckoApiKey: string,
+  ) {
+    this.client = axios.create({
+      baseURL: this.coingeckoApiUrl,
+      headers: {
+        "x-cg-demo-api-key": this.coingeckoApiKey,
+      },
+    });
+  }
 
   async getHistoricalTokenData(
     limit: number,
     offset: number,
   ): Promise<TokenHistoricalPriceResponse> {
-    return this.repo.getHistoricalNFTPrice(limit, offset);
+    const auctionPrices = await this.repo.getHistoricalNFTPrice(limit, offset);
+
+    const today = new Date();
+    const fromData = new Date(today);
+    fromData.setDate(today.getDate() - limit);
+
+    const fromQuery = fromData.toISOString().split("T")[0];
+    const toQuery = today.toISOString().split("T")[0];
+
+    const ethHistoricalPrices = await this.client.get<{
+      prices: [number, number][];
+    }>(
+      `/coins/ethereum/market_chart/range?vs_currency=usd&from=${fromQuery}&to=${toQuery}`,
+    );
+
+    const ethPriceResponse = ethHistoricalPrices.data.prices
+      .reverse()
+      .slice(0, limit);
+
+    return auctionPrices.map(({ price, timestamp }, index) => ({
+      price: (
+        Number(formatEther(BigInt(price))) * ethPriceResponse[index]![1]
+      ).toString(),
+      timestamp,
+    }));
   }
 
   async getTokenPrice(_: string, __: string): Promise<string> {
