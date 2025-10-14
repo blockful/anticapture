008a5e3000f8cfac8bade1ba5fdf4590d5515485
eduramme <eduramme@gmail.com>
2025-10-14 18:40:07 -0300

    fix: push error

--- apps/dashboard/app/[daoId]/governance/page.tsx
+++ apps/dashboard/app/[daoId]/governance/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
       ? `https://${process.env.VERCEL_URL}`
       : "http://localhost:3000");
 
-  const ogImage: Record<Exclude<DaoIdEnum, DaoIdEnum.ARBITRUM>, string> = {
+  const ogImage: Record<DaoIdEnum, string> = {
     [DaoIdEnum.ENS]: `${baseUrl}/opengraph-images/ens.png`,
     [DaoIdEnum.UNISWAP]: `${baseUrl}/opengraph-images/uni.png`,
     [DaoIdEnum.OPTIMISM]: `${baseUrl}/opengraph-images/op.png`,
@@ -32,8 +32,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   const ogDescription = `Explore and mitigate governance risks in ${daoId} DAO.`;
 
   const imageUrl =
-    ogImage[daoId as Exclude<DaoIdEnum, DaoIdEnum.ARBITRUM>] ||
-    `${baseUrl}/opengraph-images/default.png`;
+    ogImage[daoId as DaoIdEnum] || `${baseUrl}/opengraph-images/default.png`;
 
   return {
     title: ogTitle,
