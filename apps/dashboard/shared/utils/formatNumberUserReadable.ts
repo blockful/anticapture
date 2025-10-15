539462ee0974669a10069d43d6de208cfdf31b7d
lucas picollo <lucaspicolloo@gmail.com>
2025-10-15 12:03:46 -0300

    fix: token price tooltip on custom chart

--- apps/dashboard/shared/utils/formatNumberUserReadable.ts
+++ apps/dashboard/shared/utils/formatNumberUserReadable.ts
@@ -1,13 +1,13 @@
+const format = (value: number, suffix: string, fixed: number) =>
+  value.toFixed(fixed).replace(/\.?0+$/, "") + suffix;
+
 export function formatNumberUserReadable(
   num: number,
   fixed: number = 2,
 ): string {
-  const format = (value: number, suffix: string) =>
-    value.toFixed(fixed).replace(/\.?0+$/, "") + suffix;
-
-  if (num >= 1e9) return format(num / 1e9, "B"); // Billion
-  if (num >= 1e6) return format(num / 1e6, "M"); // Million
-  if (num >= 1e3) return format(num / 1e3, "K"); // Thousand
+  if (num >= 1e9) return format(num / 1e9, "B", fixed); // Billion
+  if (num >= 1e6) return format(num / 1e6, "M", fixed); // Million
+  if (num >= 1e3) return format(num / 1e3, "K", fixed); // Thousand
   return Number(num)
     .toFixed(fixed)
     .replace(/\.?0+$/, "");
