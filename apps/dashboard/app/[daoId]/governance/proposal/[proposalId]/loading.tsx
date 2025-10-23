4327b04450c51d9b3cba7e14e5b97e4f0c2750e7
eduramme <eduramme@gmail.com>
2025-10-22 22:55:27 -0300

    feat: add loading skeleton to deaful nexrt loading page

--- apps/dashboard/app/[daoId]/governance/proposal/[proposalId]/loading.tsx
+++ apps/dashboard/app/[daoId]/governance/proposal/[proposalId]/loading.tsx
@@ -1,5 +1,15 @@
 import { ProposalSectionSkeleton } from "@/features/governance/components/proposal-overview/ProposalSectionSkeleton";
 
-export default function ProposalLoading() {
-  return <ProposalSectionSkeleton />;
+export default function Loading() {
+  return (
+    <div className="bg-surface-background dark flex h-screen overflow-hidden">
+      <main className="relative flex-1 overflow-auto pt-[57px] sm:ml-[70px] sm:pt-0">
+        <div className="flex min-h-screen w-full flex-col items-center">
+          <div className="xl4k:max-w-7xl w-full flex-1">
+            <ProposalSectionSkeleton />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
