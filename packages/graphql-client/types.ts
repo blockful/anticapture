1c30c379709e93543427172b564e097b7dbfb9fe
Leonardo Vieira <jonaledi@gmail.com>
2025-11-04 09:36:04 -0300

    fix: generated files

--- packages/graphql-client/types.ts
+++ packages/graphql-client/types.ts
@@ -1743,52 +1743,6 @@ export type Query_Transactions_Items_Items_Transfers_Items = {
   transactionHash: Scalars['String']['output'];
 };
 
-export type Query_Transactions_Transactions_Items = {
-  __typename?: 'query_transactions_transactions_items';
-  delegations: Array<Maybe<Query_Transactions_Transactions_Items_Delegations_Items>>;
-  from?: Maybe<Scalars['String']['output']>;
-  isCex: Scalars['Boolean']['output'];
-  isDex: Scalars['Boolean']['output'];
-  isLending: Scalars['Boolean']['output'];
-  isTotal: Scalars['Boolean']['output'];
-  timestamp: Scalars['String']['output'];
-  to?: Maybe<Scalars['String']['output']>;
-  transactionHash: Scalars['String']['output'];
-  transfers: Array<Maybe<Query_Transactions_Items_Items_Transfers_Items>>;
-};
-
-export type Query_Transactions_Items_Items_Delegations_Items = {
-  __typename?: 'query_transactions_items_items_delegations_items';
-  daoId: Scalars['String']['output'];
-  delegateAccountId: Scalars['String']['output'];
-  delegatedValue: Scalars['String']['output'];
-  delegatorAccountId: Scalars['String']['output'];
-  isCex: Scalars['Boolean']['output'];
-  isDex: Scalars['Boolean']['output'];
-  isLending: Scalars['Boolean']['output'];
-  isTotal: Scalars['Boolean']['output'];
-  logIndex: Scalars['Float']['output'];
-  previousDelegate?: Maybe<Scalars['String']['output']>;
-  timestamp: Scalars['String']['output'];
-  transactionHash: Scalars['String']['output'];
-};
-
-export type Query_Transactions_Items_Items_Transfers_Items = {
-  __typename?: 'query_transactions_items_items_transfers_items';
-  amount: Scalars['String']['output'];
-  daoId: Scalars['String']['output'];
-  fromAccountId: Scalars['String']['output'];
-  isCex: Scalars['Boolean']['output'];
-  isDex: Scalars['Boolean']['output'];
-  isLending: Scalars['Boolean']['output'];
-  isTotal: Scalars['Boolean']['output'];
-  logIndex: Scalars['Float']['output'];
-  timestamp: Scalars['String']['output'];
-  toAccountId: Scalars['String']['output'];
-  tokenId: Scalars['String']['output'];
-  transactionHash: Scalars['String']['output'];
-};
-
 export type Query_VotingPowerVariations_Items_Items = {
   __typename?: 'query_votingPowerVariations_items_items';
   absoluteChange: Scalars['String']['output'];
@@ -2099,7 +2053,6 @@ export type Transactions_200_Response = {
   __typename?: 'transactions_200_response';
   items: Array<Maybe<Query_Transactions_Items_Items>>;
   totalCount: Scalars['Float']['output'];
-  transactions: Array<Maybe<Query_Transactions_Transactions_Items>>;
 };
 
 export type Transfer = {
