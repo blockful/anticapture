fcab9990fd73c02c884b38218694fca922146999
eduramme <eduramme@gmail.com>
2025-11-15 01:21:38 -0300

    feat: fix display for mobile

--- apps/dashboard/features/panel/PanelSection.tsx
+++ apps/dashboard/features/panel/PanelSection.tsx
@@ -10,7 +10,7 @@ import {
 export const PanelSection = () => {
   return (
     <div className="flex flex-col gap-4 px-2 py-4">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         <DaoProtectionLevels />
         <TreasuryMonitoring />
         <DelegatedSupplyHistory />
