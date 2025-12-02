668ef95b9fca415245b468036473d4dbabc06792
eduramme <eduramme@gmail.com>
2025-12-02 16:44:08 -0300

    feat: add icon styles

--- apps/dashboard/features/alerts/components/AlertCard.tsx
+++ apps/dashboard/features/alerts/components/AlertCard.tsx
@@ -9,19 +9,19 @@ export const AlertCard = ({ title, description, icon: Icon }: AlertItem) => {
         "bg-surface-background sm:bg-surface-default border-border-default w-full rounded-none border",
       )}
     >
-      <CardContent className="px-0 py-4 sm:p-4">
+      <CardContent className="sm:p-3">
         <div className="flex flex-col items-start gap-3">
           <div className="flex w-full items-center justify-between gap-2">
             <div className="flex items-center justify-center gap-2">
               <div className="flex-shrink-0">
-                <Icon className="size-5" />
+                <Icon className="size-6" />
               </div>
-              <div className="mb-1 flex items-center gap-2">
+              <div className="flex items-center gap-2">
                 <h3 className="text-primary text-sm font-medium">{title}</h3>
               </div>
             </div>
 
-            <div className="text-success bg-surface-opacity-success text-sm font-medium">
+            <div className="text-success bg-surface-opacity-success rounded-full px-[6px] py-0.5 text-sm font-medium">
               active
             </div>
           </div>
