49a660d13820929cf0f722601b641679d1097a05
eduramme <eduramme@gmail.com>
2025-12-10 12:21:43 -0300

    feat: add not-found page design

--- apps/dashboard/app/not-found.tsx
+++ apps/dashboard/app/not-found.tsx
@@ -1,16 +1,34 @@
+import { Button } from "@/shared/components";
+import { NotFoundPageIcon } from "@/shared/components/icons/NotFoundPageIcon";
+import Image from "next/image";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="text-primary flex h-screen flex-col items-center justify-center gap-4 bg-blue-500">
-      <h2 className="text-2xl font-bold">NotFound</h2>
-      <p className="text-center">NotFound</p>
-      <Link
-        href="/"
-        className="mt-4 rounded-md bg-white/10 px-4 py-2 hover:bg-white/20"
-      >
-        Return Home
-      </Link>
+    <div className="text-primary bg-surface-background flex h-screen flex-col items-center justify-center gap-4">
+      <Image
+        src="/images/bg-404.png"
+        alt="Not Found"
+        width={602}
+        height={302}
+        className="pointer-events-none absolute left-0 top-0 h-full w-full object-contain"
+      />
+      <NotFoundPageIcon />
+      <h3 className="text-primary font-['Roboto_Mono'] text-[24px] font-normal uppercase leading-8">
+        [ERROR:page_NOT_FOUND]
+      </h3>
+      <p className="text-secondary text-center font-['Inter'] text-base font-normal leading-6">
+        The system couldn&apos;t resolve the requested route. Check the URL or
+        submit a bug report.
+      </p>
+      <div className="flex items-center justify-center gap-2">
+        <Button asChild variant="primary" size="md">
+          <Link href="/dashboard">Go to Dashboard</Link>
+        </Button>
+        <Button asChild variant="outline" size="md">
+          <Link href="/">Report Issue</Link>
+        </Button>
+      </div>
     </div>
   );
 };
