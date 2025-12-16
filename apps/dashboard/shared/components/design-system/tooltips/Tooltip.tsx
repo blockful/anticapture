f0712ff162e968675064022bed0246a45b23c45f
eduramme <eduramme@gmail.com>
2025-12-15 22:05:02 -0300

    feat: fix tooltip design

--- apps/dashboard/shared/components/design-system/tooltips/Tooltip.tsx
+++ apps/dashboard/shared/components/design-system/tooltips/Tooltip.tsx
@@ -26,7 +26,7 @@ export function Tooltip({
   return (
     <Root
       open={open}
-      delayDuration={0}
+      delayDuration={300}
       onOpenChange={handleOpenChange}
       disableHoverableContent
     >
@@ -36,8 +36,8 @@ export function Tooltip({
       <Content
         data-slot="tooltip-content"
         className={cn(
-          "bg-popover text-popover-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md",
-          "bg-surface-contrast border-border-contrast text-primary max-w-[350px] border-pink-300",
+          "bg-popover text-popover-foreground z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md",
+          "bg-surface-contrast border-border-contrast text-primary max-w-[350px]",
           className,
         )}
         side="top"
