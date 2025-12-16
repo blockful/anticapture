ebaf77d8635cf680b64e14fe79e71e373e11c97b
eduramme <eduramme@gmail.com>
2025-12-15 21:04:01 -0300

    refactor: get rid of tooltip plain

--- apps/dashboard/shared/components/design-system/tooltips/Tooltip.tsx
+++ apps/dashboard/shared/components/design-system/tooltips/Tooltip.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  Tooltip as TooltipPrimitive,
-  TooltipTrigger,
-  TooltipContent,
-} from "@/shared/components/ui/tooltip";
+import { Content, Trigger, Root } from "@radix-ui/react-tooltip";
 import { cn } from "@/shared/utils/";
 import { ReactNode, useState } from "react";
 
@@ -28,26 +24,29 @@ export function Tooltip({
   };
 
   return (
-    <TooltipPrimitive open={open} onOpenChange={handleOpenChange}>
-      <TooltipTrigger
-        role="button"
-        aria-label="tooltip-info"
-        onClick={handleToggle}
-      >
+    <Root open={open} onOpenChange={handleOpenChange}>
+      <Trigger role="button" aria-label="tooltip-info" onClick={handleToggle}>
         {children}
-      </TooltipTrigger>
-      <TooltipContent
+      </Trigger>
+      <Content
+        data-slot="tooltip-content"
+        className={cn(
+          "bg-popover text-popover-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm shadow-md",
+          "bg-surface-contrast border-border-contrast text-primary max-w-[350px] border-pink-300",
+          className,
+        )}
         side="top"
         align="center"
         sideOffset={10}
         avoidCollisions={true}
-        className={cn(
-          "bg-surface-contrast border-border-contrast text-primary max-w-[350px]",
-          className,
-        )}
       >
         {tooltipContent}
-      </TooltipContent>
-    </TooltipPrimitive>
+      </Content>
+    </Root>
   );
 }
+
+// TooltipPlainStyles
+// "border-light-dark bg-surface-default text-primary z-50 rounded-lg border p-3 text-center shadow-sm",
+// "w-fit max-w-[calc(100vw-2rem)] sm:max-w-md",
+// "whitespace-normal break-words",
