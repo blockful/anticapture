41b7c0c594118cda6638ddecff17334e5dec1278
Leonardo Vieira <jonaledi@gmail.com>
2025-12-18 16:21:27 -0300

    feat: treasury interfaces and types

--- apps/indexer/src/api/services/treasury/types.ts
+++ apps/indexer/src/api/services/treasury/types.ts
@@ -1,4 +1,29 @@
+/**
+ * Interface to represent a treasury's data point
+ */
 export interface TreasuryDataPoint {
   date: bigint; // Unix timestamp in seconds (start of day)
   liquidTreasury: number;
+  tokenTreasury?: number;
+  totalTreasury?: number;
+}
+
+/**
+ * Enum to set the type of treasury
+ */
+export enum TreasuryType {
+  LIQUID = "liquid",
+  DAO_TOKEN = "dao-token",
+  TOTAL = "total",
+}
+
+/**
+ * Treasury's response to the client
+ */
+export interface TreasuryResponse {
+  items: {
+    value: number;
+    date: number;
+  }[];
+  totalCount: number;
 }
