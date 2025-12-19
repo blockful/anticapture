726062aa4686c6edcea488ec65205954a4373c9a
Leonardo Vieira <jonaledi@gmail.com>
2025-12-19 11:29:31 -0300

    refactor: types.ts

--- apps/dashboard/shared/dao-config/types.ts
+++ apps/dashboard/shared/dao-config/types.ts
@@ -4,7 +4,6 @@ import { DaoIdEnum } from "@/shared/types/daos";
 import { MetricTypesEnum } from "@/shared/types/enums/metric-type";
 import { RiskLevel, GovernanceImplementationEnum } from "@/shared/types/enums";
 import { DaoIconProps } from "@/shared/components/icons/types";
-import { TreasuryAssetNonDaoToken } from "@/features/attack-profitability/hooks";
 
 export type DaoMetricsDayBucket = {
   date: string;
@@ -185,8 +184,6 @@ export interface DaoAddresses {
 
 export interface AttackProfitabilityConfig {
   riskLevel?: RiskLevel;
-  liquidTreasury?: TreasuryAssetNonDaoToken; // FIXME(DEV-161): Remove once treasury fetching from Octav is operational
-  supportsLiquidTreasuryCall?: boolean;
   attackCostBarChart: DaoAddresses[DaoIdEnum];
   dynamicQuorum?: {
     percentage: number;
