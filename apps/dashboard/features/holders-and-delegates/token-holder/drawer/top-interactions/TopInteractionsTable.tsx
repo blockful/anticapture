6c3dea3cfe443d37a77e1878aaebc2e5c6d532e9
lucas picollo <lucaspicolloo@gmail.com>
2026-01-05 16:50:14 -0300

    fix: copy address from table

--- apps/dashboard/features/holders-and-delegates/token-holder/drawer/top-interactions/TopInteractionsTable.tsx
+++ apps/dashboard/features/holders-and-delegates/token-holder/drawer/top-interactions/TopInteractionsTable.tsx
@@ -141,7 +141,7 @@ export const TopInteractionsTable = ({
             />
             <div className="flex items-center opacity-0 transition-opacity [tr:hover_&]:opacity-100">
               <CopyAndPasteButton
-                textToCopy={address as `0x${string}`}
+                textToCopy={addressValue as `0x${string}`}
                 customTooltipText={{
                   default: "Copy address",
                   copied: "Address copied!",
