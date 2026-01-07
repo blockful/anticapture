0af72f22d704bb5bacc267d2a3974422b1a9da8b
eduramme <eduramme@gmail.com>
2026-01-07 16:56:06 -0300

    feat: create delegation mappers

---
+++ apps/indexer/src/api/mappers/delegations/index.ts
@@ -0,0 +1,68 @@
+import { z } from "@hono/zod-openapi";
+
+// where: {
+//     daoId: $daoId
+//     delegatorAccountId_in: $delegator
+//     delegateAccountId: $delegate
+//   }
+
+// (
+//     where: {
+//       delegatorAccountId: $delegator
+//       delegateAccountId: $delegate
+//       delegatedValue_gte: $minDelta
+//       delegatedValue_lte: $maxDelta
+//     }
+//     orderBy: $orderBy
+//     orderDirection: $orderDirection
+//     limit: $limit
+//     after: $after
+//     before: $before
+//   ) {
+
+// (where: { delegatorAccountId: $delegator })
+
+export const DelegationsRequestSchema = z.object({
+  delegatorAccountId: z.number().optional(),
+  delegateAccountId: z.number().optional(),
+  minDelta: z.number().optional(),
+  maxDelta: z.number().optional(),
+
+  // pagination
+  skip: z.number().optional(),
+  limit: z.number().optional(),
+
+  orderBy: z.string().optional(),
+  orderDirection: z.string().optional(),
+});
+
+// items{
+//     delegatorAccountId
+//     timestamp
+// totalCount
+//       }
+
+// items {
+//     delegateAccountId
+//     delegatedValue
+//     timestamp
+//     transactionHash
+//   }
+//   pageInfo {
+//     hasNextPage
+//     hasPreviousPage
+//     startCursor
+//     endCursor
+//   }
+
+export const DelegationsResponseSchema = z.object({
+  delegatorAccountId: z.string(),
+  delegateAccountId: z.string(),
+  delegatedValue: z.string(),
+  transactionHash: z.string(),
+  timestamp: z.string(),
+  totalCount: z.number(),
+
+  skip: z.number(),
+  limit: z.number(),
+});
