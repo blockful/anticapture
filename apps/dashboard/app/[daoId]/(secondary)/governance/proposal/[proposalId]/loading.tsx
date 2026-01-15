962b5e36361e5717ce58d7656e15ddcfddeb1843
eduramme <eduramme@gmail.com>
2026-01-15 09:13:04 -0300

    fix: governance proposal skeleton

--- apps/dashboard/app/[daoId]/(secondary)/governance/proposal/[proposalId]/loading.tsx
+++ apps/dashboard/app/[daoId]/(secondary)/governance/proposal/[proposalId]/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
     <div className="bg-surface-background dark flex h-screen overflow-hidden">
       <main className="relative flex-1 overflow-auto pt-[57px] sm:pt-0">
         <div className="flex min-h-screen w-full flex-col items-center">
-          <div className="xl4k:max-w-7xl w-full flex-1">
+          <div className="w-full max-w-screen-2xl flex-1">
             <ProposalSectionSkeleton />
           </div>
         </div>
