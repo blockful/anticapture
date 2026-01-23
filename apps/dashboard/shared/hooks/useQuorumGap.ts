2b70b8eb425f125bdd0f1d0160ae239497af4115
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-01-23 15:35:00 -0300

    fix: oops

--- apps/dashboard/shared/hooks/useQuorumGap.ts
+++ apps/dashboard/shared/hooks/useQuorumGap.ts
@@ -34,7 +34,7 @@ query GetDaoData {
   dao {
     quorum
   }
-  proposals(limit: ${limit}, fromDate: '${cutoffDate}') {
+  proposals(limit: ${limit}, fromDate: "${cutoffDate}") {
     items {
       timestamp
     }
