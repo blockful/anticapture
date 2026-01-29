8361c45a0c95ec2bec2bd342b69c4caa94757495
eduramme <eduramme@gmail.com>
2026-01-28 23:35:10 -0300

    feat: reduce gap between cards to 4

--- apps/dashboard/features/governance/components/proposal-overview/ProposalSection.tsx
+++ apps/dashboard/features/governance/components/proposal-overview/ProposalSection.tsx
@@ -74,7 +74,7 @@ export const ProposalSection = () => {
         <div className="bg-surface-background sticky top-[65px] z-10 hidden h-5 w-full lg:block" />
 
         <div className="flex flex-col gap-6 p-5 lg:flex-row lg:pt-0">
-          <div className="self-star left-0 top-5 flex h-fit w-full flex-col gap-6 lg:sticky lg:top-[85px] lg:w-[420px]">
+          <div className="self-star left-0 top-5 flex h-fit w-full flex-col gap-4 lg:sticky lg:top-[85px] lg:w-[420px]">
             <TitleSection proposal={proposal} />
             <ProposalInfoSection proposal={proposal} decimals={decimals} />
             <ProposalStatusSection proposal={proposal} />
