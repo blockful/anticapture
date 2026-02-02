de0f0cf11010de3ca26083cebf892da202bcc515
eduramme <eduramme@gmail.com>
2026-02-02 20:23:46 -0300

    feat: remove border from header

--- apps/dashboard/features/governance/components/proposal-overview/ProposalSectionSkeleton.tsx
+++ apps/dashboard/features/governance/components/proposal-overview/ProposalSectionSkeleton.tsx
@@ -4,7 +4,7 @@ export const ProposalSectionSkeleton = () => {
   return (
     <div>
       {/* Header Skeleton */}
-      <div className="border-border-default mx-auto flex h-[65px] w-full shrink-0 items-center justify-between gap-6 border-b px-5 py-2">
+      <div className="mx-auto flex h-[65px] w-full shrink-0 items-center justify-between gap-6 px-5 py-2">
         <div className="m-auto flex w-full max-w-screen-2xl items-center justify-between border">
           <div className="flex items-center gap-2">
             <SkeletonRow
