5053ca2a85e92901f66fd2f5013fe2cc686b85cb
lucas picollo <lucaspicolloo@gmail.com>
2026-03-02 15:31:32 -0300

    chore: aave config typo

--- apps/indexer/config/aave.config.ts
+++ apps/indexer/config/aave.config.ts
@@ -9,7 +9,7 @@ export default createConfig({
     connectionString: env.DATABASE_URL,
   },
   chains: {
-    zksync_mainnet: {
+    ethereum_mainnet: {
       id: 1,
       rpc: env.RPC_URL,
       maxRequestsPerSecond: env.MAX_REQUESTS_PER_SECOND,
@@ -19,19 +19,19 @@ export default createConfig({
   contracts: {
     AAVE: {
       abi: AaveAbi,
-      chain: "zksync_mainnet",
+      chain: "ethereum_mainnet",
       address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
       startBlock: 10926829,
     },
     stkAAVE: {
       abi: stkAave,
-      chain: "zksync_mainnet",
+      chain: "ethereum_mainnet",
       address: "0x4da27a545c0c5B758a6BA100e3a049001de870f5",
       startBlock: 10927018,
     },
     aAAVE: {
       abi: aAave,
-      chain: "zksync_mainnet",
+      chain: "ethereum_mainnet",
       address: "0xA700b4eB416Be35b2911fd5Dee80678ff64fF6C9",
       startBlock: 16496810,
     },
