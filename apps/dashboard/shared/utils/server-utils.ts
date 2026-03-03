b06d2c0df9648ab47247bd06a657e9a5075b0ae1
lucas picollo <lucaspicolloo@gmail.com>
2026-03-03 14:54:00 -0300

    refactor: rename dash token

--- apps/dashboard/shared/utils/server-utils.ts
+++ apps/dashboard/shared/utils/server-utils.ts
@@ -1,28 +1,7 @@
-import { Address } from "viem";
-
-import { publicClient } from "@/shared/services/wallet/wallet";
-
-export type User = {
-  walletAddress: Address;
-  ensName: string | null;
-};
-
 export const BACKEND_ENDPOINT = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getAuthHeaders = (): Record<string, string> => ({
-  ...(process.env.NEXT_PUBLIC_API_TOKEN && {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+  ...(process.env.NEXT_PUBLIC_BLOCKFUL_API_TOKEN && {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_BLOCKFUL_API_TOKEN}`,
   }),
 });
-
-export const bulkGetEnsName = async (addresses: Address[]) => {
-  const names = [];
-
-  for (let i = 0; i < addresses.length; i++) {
-    let ensName = await publicClient.getEnsName({ address: addresses[i] });
-
-    names.push(ensName);
-  }
-
-  return names;
-};
