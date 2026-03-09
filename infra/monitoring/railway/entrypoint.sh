225cbb902e5cab6575ae293f3818c4bad3f4e475
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-03-09 16:56:41 -0300

    fix: alertmanager configuration

--- infra/monitoring/railway/entrypoint.sh
+++ infra/monitoring/railway/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g" /etc/alertmanager/config.yml.tmpl > /etc/alertmanager/config.yml
-exec /bin/alertmanager --config.file=/etc/alertmanager/config.yml --storage.path=/alertmanager "$@"
+sed "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g;s|\${SLACK_CHANNEL}|${SLACK_CHANNEL}|g" /etc/alertmanager/config.yml.tmpl > /etc/alertmanager/config.yml
+exec /bin/alertmanager --config.file=/etc/alertmanager/config.yml --storage.path=/alertmanager ${ALERTMANAGER_EXTERNAL_URL:+--web.external-url="$ALERTMANAGER_EXTERNAL_URL"} "$@"
