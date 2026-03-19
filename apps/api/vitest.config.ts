099c738981cc59d478fc908bb5f599b0982660af
Leonardo Vieira <jonaledi@gmail.com>
2026-03-19 09:24:07 -0300

    feat: exclue on coverage

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -5,6 +5,21 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: [
+        "src/**/*.test.ts",
+        "src/**/*.d.ts",
+        "src/clients/*/abi/**",
+        "src/database/schema.ts",
+        "src/database/offchain-schema.ts",
+        "src/**/*.interface.ts",
+        "src/**/types.ts",
+        "src/env.ts",
+        "src/docs.ts",
+      ],
+    },
   },
   resolve: {
     alias: {
