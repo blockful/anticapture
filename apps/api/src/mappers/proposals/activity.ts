d0c5f13d1aae64bd15f3dd965df566ebf398477d
Leonardo Vieira <jonaledi@gmail.com>
2026-03-23 15:14:05 -0300

    refactor: use transform instead of coerce

--- apps/api/src/mappers/proposals/activity.ts
+++ apps/api/src/mappers/proposals/activity.ts
@@ -39,7 +39,10 @@ export const ProposalActivityResponseSchema = z.object({
           voterAccountId: z.string(),
           proposalId: z.string(),
           support: z.string().nullable(),
-          votingPower: z.coerce.string(),
+          votingPower: z
+            .bigint()
+            .transform((val) => val.toString())
+            .openapi({ type: "string" }),
           reason: z.string().nullable(),
           timestamp: z.coerce.string(),
         })
