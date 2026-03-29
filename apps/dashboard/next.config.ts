c00c55c72597ad7b48d6e2691d23f51c164f490f
lucas picollo <lucaspicolloo@gmail.com>
2026-03-29 18:26:38 -0300

    fix: typo turbopack

--- apps/dashboard/next.config.ts
+++ apps/dashboard/next.config.ts
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@anticapture/graphql-client"],
+  turbopack: {
+    resolveAlias: {
+      "@anticapture/graphql-client/hooks":
+        "@anticapture/graphql-client/generated/hooks.ts",
+      "@anticapture/graphql-client":
+        "@anticapture/graphql-client/generated/hooks.ts",
+    },
+  },
   images: {
     remotePatterns: [
       {
