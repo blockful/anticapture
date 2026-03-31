caa1f4c8438cbc8f8a009aa191617ab9af760119
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-03-31 17:16:00 -0300

    fix: path

--- infra/monitoring/entrypoint.loki.sh
+++ infra/monitoring/entrypoint.loki.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /loki/chunks /loki/wal
 chown -R 10001:10001 /loki
-exec su-exec 10001 /usr/bin/loki -config.file=/etc/loki/local-config.yaml -config.expand-env=true "$@"
+exec /sbin/su-exec 10001 /usr/bin/loki -config.file=/etc/loki/local-config.yaml -config.expand-env=true "$@"
