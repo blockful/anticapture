c9f742309fd5119b9be2f75319f9ae1f4e3f50af
Leonardo Vieira <jonaledi@gmail.com>
2026-04-06 12:52:35 -0300

    refactor: is address validation on schema

--- apps/offchain-indexer/src/mappers/proposal.ts
+++ apps/offchain-indexer/src/mappers/proposal.ts
@@ -1,10 +1,13 @@
-import { getAddress } from "viem";
+import { getAddress, isAddress } from "viem";
 import { z } from "zod";
 
 // "nullish().transform" is necessary here because provider return null instead of undefined
 export const rawProposalSchema = z.object({
   id: z.string(),
-  author: z.string().transform((val) => getAddress(val)),
+  author: z
+    .string()
+    .refine((val) => isAddress(val, { strict: false }))
+    .transform((val) => getAddress(val)),
   title: z.string(),
   body: z
     .string()
