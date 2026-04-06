89fdeefeec489f5de021c9d30fe8da072d5710e1
lucas picollo <lucaspicolloo@gmail.com>
2026-04-06 13:06:47 -0300

    chore: fix storybook build 3

--- apps/dashboard/.storybook/main.ts
+++ apps/dashboard/.storybook/main.ts
@@ -39,7 +39,7 @@ const config: StorybookConfig = {
       "@apollo/client": dirname(
         require.resolve("@apollo/client/package.json"),
       ),
-      "@anticapture/graphql-client": resolve(
+      "@anticapture/graphql-client$": resolve(
         __dirname,
         "../../../packages/graphql-client/generated/hooks.ts",
       ),
