f4f9b61ca58954586e3a6a982363a5e8f4add907
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-04-24 14:55:11 -0300

    fix: sentinel file

--- infra/monitoring/entrypoint.prometheus.sh
+++ infra/monitoring/entrypoint.prometheus.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 envsubst < /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
-rm -rf /prometheus/*
+if [ ! -f /prometheus/.initialized ]; then
+  rm -rf /prometheus/*
+  touch /prometheus/.initialized
+fi
 exec /bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.enable-lifecycle --web.enable-remote-write-receiver "$@"
