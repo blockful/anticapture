128df95ef5bf8cd79e71c8b94615d6c90c7efb6f
Pedro Binotto <pedrosantibinotto@gmail.com>
2026-05-20 14:48:20 -0300

    test: add self-metrics to observability setup

--- infra/monitoring/entrypoint.alertmanager.sh
+++ infra/monitoring/entrypoint.alertmanager.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 sed "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g;s|\${SLACK_CHANNEL}|${SLACK_CHANNEL}|g" /etc/alertmanager/config.yml.tmpl > /etc/alertmanager/config.yml
-exec /bin/alertmanager --config.file=/etc/alertmanager/config.yml --storage.path=/alertmanager ${ALERTMANAGER_EXTERNAL_URL:+--web.external-url="$ALERTMANAGER_EXTERNAL_URL"} "$@"
+exec /bin/alertmanager \
+  --config.file=/etc/alertmanager/config.yml \
+  --storage.path=/alertmanager \
+  --log.level="${ALERTMANAGER_LOG_LEVEL:-debug}" \
+  --log.format="${ALERTMANAGER_LOG_FORMAT:-json}" \
+  ${ALERTMANAGER_EXTERNAL_URL:+--web.external-url="$ALERTMANAGER_EXTERNAL_URL"} \
+  "$@"
--- infra/monitoring/entrypoint.prometheus.sh
+++ infra/monitoring/entrypoint.prometheus.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 envsubst < /etc/prometheus/prometheus.yml.tmpl > /etc/prometheus/prometheus.yml
-exec /bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.enable-lifecycle --web.enable-remote-write-receiver "$@"
+exec /bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/prometheus \
+  --web.enable-lifecycle \
+  --web.enable-remote-write-receiver \
+  --log.level="${PROMETHEUS_LOG_LEVEL:-debug}" \
+  --log.format="${PROMETHEUS_LOG_FORMAT:-json}" \
+  "$@"
